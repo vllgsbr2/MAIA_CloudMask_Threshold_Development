7209e7c5d313713d767dcfdbccf54e83d7892d17
Villegas Bravo <vllgsbr2@illinois.edu>
2020-01-16 13:56:43 -0600

    sreg

--- scripts/check_not_processed.py
+++ scripts/check_not_processed.py
@@ -1,26 +1,28 @@
 import pandas as pd
 import os
-
+import numpy as np
 #get txt files
 home = '/data/keeling/a/vllgsbr2/c/MAIA_thresh_dev/MAIA_CloudMask_Threshold_Development/scripts/MPI_create_dataset_output'
 files = os.listdir(home)
 
 #master dataframe to store all time stamps processed
-master_df = pd.DataFrame()
-master_df.columns = ['time_stamps']
+master_df = pd.DataFrame(data=None, columns=['time_stamps'], dtype=str)
+#master_df.columns = ['time_stamps']
 
-for file in files:
-    temp_df = pd.read_csv(file)
-    temp_df.columns = ['num', 'time_stamp', 'phrase']
+for f in files:
+    try:
+        temp_df = pd.read_csv(home+'/'+f, dtype=str)
+        temp_df.columns = ['num', 'time_stamp', 'phrase']
 
-    master_df.append(temp_df['time_stamp'])
-
+        master_df['time_stamps'] =  master_df['time_stamps'].append(temp_df['time_stamp'])
+    except:
+        pass
 processed_files = list(master_df['time_stamps'])
-
+#print(master_df)
 home = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/'
-files = 'LAADS_query.2019-10-15T18_07.csv'
+f = 'LAADS_query.2019-10-15T18_07.csv'
 
-laads_query_df = pd.read_csv(home+file)
+laads_query_df = pd.read_csv(home+f)
 file_name_column = 'fileUrls from query MOD021KM--61 MOD03--61 MOD35_L2'\
                    '--61 2002-01-01..2019-10-15 x-124.4y39.8 x-112.8y30.7[5]'
 all_files = list(laads_query_df[file_name_column])
@@ -28,7 +30,7 @@ all_files = list(laads_query_df[file_name_column])
 # check_df = pd.DataFrame()
 # check_df.columns = ['time_stamps', 'processed_1_not_processed_0']
 
-check = open('./check_processed.csv')
+check = open('./check_processed.csv', 'w')
 check.write('time_stamps,processed_1_not_processed_0\n')
 
 #choose PTA from keeling
@@ -50,15 +52,18 @@ filename_MOD_35 = np.sort(filename_MOD_35)
 time_stamps_downloaded = [x[10:22] for x in filename_MOD_02]
 # filename_MOD_03_timeStamp = [x[7:19]  for x in filename_MOD_03]
 # filename_MOD_35_timeStamp = [x[10:22] for x in filename_MOD_35]
-
+counter = 0
 for i in time_stamps_downloaded:
     found = False
     for j in processed_files:
-        if i==j:
-            check.write('{},{}\n'.format(i, 1)
+        #print(i+'\n'+j[1:])
+        if str(i)==str(j[1:]):
+            check.write('{},{}\n'.format(i, 1))
             found = True
+            counter+=1
             break
     if not found:
         check.write('{},{}\n'.format(i, 0))
-
+print(counter)
 check.close()
+print(len(master_df))
