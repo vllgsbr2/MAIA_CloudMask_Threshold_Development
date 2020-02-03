8b6bb39b0d7558506aebaa0589fc0bef41393581
Villegas Bravo <vllgsbr2@illinois.edu>
2020-02-03 13:24:43 -0600

    operational on keeling

--- scripts/obs_vs_thresh_plots.py
+++ scripts/obs_vs_thresh_plots.py
@@ -3,7 +3,7 @@ import h5py
 import os
 import matplotlib.pyplot as plt
 
-# home = ''
+home = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/LA_PTA_MODIS_Data/try2_database/'
 #
 # #Make scatter plot NDVI/NDSI vs whiteness index with a 1 to 1 line
 # #testing independence
@@ -21,32 +21,44 @@ import matplotlib.pyplot as plt
 grouped_list = os.listdir(home + 'test_thresholds/')
 #'cosSZA_00_VZA_00_RAZ_00_TA_00_DOY_00_sceneID_00'
 #don't count TA and DOY since its the same for all thresholds
-n=13n+
-OLP = [[int(group[n+7:n+9])  ,\
-        int(group[n+14:n+16]),\
-        int(group[n+21:n+23]),\
-        int(group[n+45:])] ,\
+n=13
+OLP = []
 
-        for group in grouped_list] #(6, num groups)
+for group in grouped_list: #(6, num groups)
+    OLP.append([int(group[n+7:n+9])  ,\
+                 int(group[n+14:n+16]),\
+                 int(group[n+21:n+23]),\
+                 int(group[n+45:n+47])     ])
+OLP = np.array(OLP)
 
-grouped_list = [home + x for x in grouped_list]
+OLP[OLP==30] = 12
+OLP[OLP==31] = 13
+OLP[OLP==32] = 14
+
+grouped_list = [home +'test_thresholds/'+ x for x in grouped_list]
 
 thresholds = np.zeros((7, len(grouped_list))) #thresh per obs, num total thresh
 
 for i, group in enumerate(grouped_list):
     with h5py.File(group, 'r') as hf_group:
-        thresholds[:,i] = hf_group['threhsolds'][()]
+        try:
+            thresholds[:,i] = hf_group['thresholds'][()]
+        except:
+            thresholds[:,i]=np.nan
+#hist = np.zeros((7, 50, 15))
+#hist
 
-hist = np.zeros((7, 50, 15))
-hist
+thresholds[thresholds<0] = np.nan
 
 f, axes = plt.subplots(ncols=7)
 cmap = 'jet'
 OLP_labels = ['cos(SZA)', 'VZA', 'RAZ', 'Scene ID']
-    for i, ax in enumerate(axes):
-        #np.histogram(threshold[i,:], bins=50, range=(0,1))
-        ax.scatter(threshold[i,:], OLP[-1,:])
-        ax.set_xlabel('{} Thresholds'.format())
-        ax.set_ylabel('{}'.format(OLP_labels[-1]))
-
+obs = ['WI', 'NDVI', 'NDSI', 'visRef', 'nirRef', 'SVI', 'cirrus']
+for i, ax in enumerate(axes):
+    #np.histogram(threshold[i,:], bins=50, range=(0,1))
+    #print(OLP.shape)
+    ax.scatter(OLP[:,1 ], thresholds[i,:])
+    ax.set_ylabel('{} Thresholds'.format(obs[i]))
+    ax.set_xlabel('{}'.format(OLP_labels[1]))
+plt.tight_layout()
 plt.show()
