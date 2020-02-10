0d212da36c9f3ca45e3d7f7164435a1d4f84fb37
vllgsbr2 <vllgsbr2@illinois.edu>
2020-02-10 17:02:07 -0600

    new file structure for MCM to read

--- scripts/calc_threshold.py
+++ scripts/calc_threshold.py
@@ -1,140 +1,3 @@
-# import numpy as np
-# import h5py
-#
-# def calc_thresh(group_file):
-#     '''
-#     Objective:
-#         Takes in grouped_data_cosSZA_08_VZA_09_RAZ_08_TA_01_DOY_05_sceneID_09.hdf5
-#         files. Inside are a data points containing the cloud mask and observables
-#         for that pixel. The OLP is in the file name. The threshold is then
-#         calculated for that group and saved into a threshold file
-#     Arguments:
-#         group_file {str} -- contains data points to calc threshold for that OLP
-#     Return:
-#         void
-#     '''
-#
-#     with h5py.File(group_file, 'r+') as hf_group:
-#         hf_keys    = list(hf_group.keys())
-#         num_points = len(hf_keys)
-#         cloud_mask = np.zeros((num_points))
-#
-#         observables = ['WI', 'NDVI', 'NDSI', 'visRef', 'nirRef', 'SVI', 'cirrus']
-#         obs = np.empty((num_points, 7))
-#         thresholds = np.zeros((7))
-#
-#         for i, data_point in enumerate(hf_keys):
-#             dataset_path  = '{}/label_and_obs'.format(data_point)
-#             try:
-#                 data = hf_group[dataset_path][()]
-#                 cloud_mask[i] = int(data[0])
-#                 obs[i,:]      = data[1:]
-#             except:
-#                 cloud_mask[i] = np.nan#int(data[0])
-#                 obs[i,:]      = np.nan#data[1:]
-#
-#         clear_idx = np.where(cloud_mask != 0)
-#         clear_obs = obs[clear_idx[0],:]
-#
-#         cloudy_idx = np.where(cloud_mask != 0)
-#         cloudy_obs = obs[cloudy_idx[0],1:3] #[1:3] since we only need for NDxI
-#
-#         for i in range(7):
-#             #WI
-#             if i==0:
-#                 thresholds[i] = np.nanpercentile(clear_obs[:,i], 1)
-#             #NDxI
-#             #pick max from cloudy hist
-#             elif i==1 or i==2:
-#                 thresholds[i] = np.hist(cloudy_obs[:,i], bins=50, range=(-1,1))[0].max()
-#             #VIS/NIR/SVI/Cirrus
-#             else:
-#                 thresholds[i] = np.nanpercentile(clear_obs[:,i], 99)
-#
-#         try:
-#             dataset = hf_group.create_dataset('thresholds', data=thresholds)
-#
-#             #label the data
-#             dataset.attrs['threshold labels'] = 'WI, NDVI, NDSI, visRef,\
-#                                nir_Ref, SVI, cirrus'
-#         except:
-#             hf_group['thresholds'][:] = thresholds
-#
-#         print(group_file)
-#
-# if __name__ == '__main__':
-#
-#     import h5py
-#     import mpi4py.MPI as MPI
-#     import tables
-#     import os
-#     tables.file._open_files.close_all()
-#
-#     comm = MPI.COMM_WORLD
-#     rank = comm.Get_rank()
-#     size = comm.Get_size()
-#
-#     for r in range(size):
-#         if rank==r:
-#
-#             #define paths for the three databases
-#             home = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/LA_PTA_MODIS_Data/try2_database/'
-#             PTA_file_path    = home + 'group_DOY_05_60_cores/'
-#             database_files   = os.listdir(PTA_file_path)
-#             database_files   = [PTA_file_path + filename for filename in database_files]
-#             database_files   = np.sort(database_files)
-#
-#             #define start and end file for a particular rank
-#             #(size - 1) so last processesor can take the modulus
-#             num_files        = len(database_files)
-#             files_per_cpu    = num_files//(size-1)
-#             start, end       = r*files_per_cpu, (r+1)*files_per_cpu
-#
-#             if r==(size-1):
-#                 start, end = r*files_per_cpu, r*files_per_cpu + num_files % size
-#
-#             hf_group_paths = database_files[start:end]
-#
-#             for path in hf_group_paths:
-#                 calc_thresh(path)
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# #*********************************************************************
-
-
-
 import numpy as np
 import h5py
 
@@ -156,15 +19,19 @@ def calc_thresh(group_file):
     with h5py.File(group_file, 'r+') as hf_group,\
          h5py.File(home + '/thresholds.hdf5', 'w') as hf_thresh:
 
+        #cosSZA_00_VZA_00_RAZ_00_TA_00_sceneID_00_DOY_00
+        TA_group  = hf_thresh.create_group(bin_ID[24:29])
+        DOY_group = TA_group.create_group(bin_ID[-6:])
+
         hf_keys    = list(hf_group.keys())
         num_points = len(hf_keys)
 
-        for count, bin in enumerate(hf_keys):
+        for count, bin_ID in enumerate(hf_keys):
             #observables = ['WI', 'NDVI', 'NDSI', 'visRef', 'nirRef', 'SVI', 'cirrus']
             thresholds = np.zeros((7))
 
-            cloud_mask = hf_group[bin][:,0].astype(dtype=np.int)
-            obs        = hf_group[bin][:,1:]
+            cloud_mask = hf_group[bin_ID][:,0].astype(dtype=np.int)
+            obs        = hf_group[bin_ID][:,1:]
 
 
             clear_idx = np.where(cloud_mask != 0)
@@ -180,54 +47,38 @@ def calc_thresh(group_file):
                 #NDxI
                 #pick max from cloudy hist
                 elif i==1 or i==2:
-                    hist, bin_edges = np.histogram(cloudy_obs[:,i-1], bins=128, range=(-1,1)) 
-                    thresholds[i]   = bin_edges[1:][hist==hist.max()].min() 
+                    hist, bin_edges = np.histogram(cloudy_obs[:,i-1], bins=128, range=(-1,1))
+                    thresholds[i]   = bin_edges[1:][hist==hist.max()].min()
                 #VIS/NIR/SVI/Cirrus
                 else:
                     thresholds[i] = np.nanpercentile(clear_obs[:,i], 99)
 
+            dataset_name = bin_ID[:23]+bin_ID[-18:-7]
+
+            #could be an alternative to the try and except statement below
+            #h5py.require_dataset(dataset_name)
+
             try:
-                dataset = hf_thresh.create_dataset(bin, data=thresholds)
+                dataset = DOY_group.create_dataset(bin_ID[:23]+bin_ID[-18:-7], data=thresholds)
 
                 #label the data
                 dataset.attrs['threshold labels'] = 'WI, NDVI, NDSI, visRef,\
-                                   nir_Ref, SVI, cirrus'
+                                                     nir_Ref, SVI, cirrus'
             except:
-                hf_thresh[bin][:] = thresholds
+                path = '{}/{}/{}'.format(bin_ID[24:29], bin_ID[-6:], bin_ID[:23]+bin_ID[-18:-7])
+                hf_thresh[path][:] = thresholds
 
-            print(count, thresholds)
+            #print(count, thresholds)
 
 if __name__ == '__main__':
 
     import h5py
-    #import mpi4py.MPI as MPI
     import tables
     import os
     tables.file._open_files.close_all()
-
-    # comm = MPI.COMM_WORLD
-    # rank = comm.Get_rank()
-    # size = comm.Get_size()
-    #
-    # for r in range(size):
-    #     if rank==r:
 
     #define paths for the database
     home = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/LA_PTA_MODIS_Data/try2_database/'
     grouped_file_path    = home + 'grouped_obs_and_CM.hdf5'
 
     calc_thresh(grouped_file_path)
-
-            # #define start and end file for a particular rank
-            # #(size - 1) so last processesor can take the modulus
-            # num_files        = len(database_files)
-            # files_per_cpu    = num_files//(size-1)
-            # start, end       = r*files_per_cpu, (r+1)*files_per_cpu
-            #
-            # if r==(size-1):
-            #     start, end = r*files_per_cpu, r*files_per_cpu + num_files % size
-            #
-            # hf_group_paths = database_files[start:end]
-            #
-            # for path in hf_group_paths:
-            #     calc_thresh(path)
