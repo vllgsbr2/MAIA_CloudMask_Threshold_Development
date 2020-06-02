5551d45ccc417487dc3895838badd5d4456e4e73
vllgsbr2 <vllgsbr2@illinois.edu>
2020-06-02 16:30:26 -0500

    plot and calc scene accuracy time series

---
+++ test_thresholds/calc_scene_accuracy_all_DOY.py
@@ -0,0 +1,76 @@
+
+def scene_conf_matx(conf_matx_path, DOY_bin, result_path):
+
+    with h5py.File(conf_matx_path, 'r') as hf_confmatx:
+        confmatx_keys = np.array(list(hf_confmatx.keys()))
+        time_stamps   = [x[-12:] for x in confmatx_keys]
+        masks         = [x for x in confmatx_keys if x[:-13] == 'confusion_matrix_mask']
+        tables        = [x for x in confmatx_keys if x[:-13] == 'confusion_matrix_table']
+        #print(masks)
+        #pixel by pixel accuracy
+        accuracy = np.zeros((1000,1000,4))
+        #number of samples that contributed to every evaluation type
+        #needed since not all pixels in each scene have data, -999 instead
+        num_samples = np.zeros((1000,1000,4))
+
+        for i, mask in enumerate(masks):
+            mask = hf_confmatx[mask][()]
+            present_data_idx = np.where((mask != -999) & (mask != 0))
+            no_data_idx    = np.where(mask == -999)
+            #set mask to zero for no data so it doesnt contribute to accuracy
+            mask[no_data_idx] = 0
+            #add count to num samples where data is present
+            num_samples[present_data_idx] += 1
+
+            # xx = np.shape(present_data_idx)[1]/1e6
+            # print(time_stamps[i], xx)
+
+            accuracy += mask
+
+        #print(accuracy[:,:,0])
+        #% of time mask is correct at each location, when data is present
+        MCM_accuracy = np.sum(accuracy[:,:,:2], axis=2) / np.sum(num_samples, axis=2)#num_samples[:,:,0]
+
+        #write MCM_accuracy and num_samples to disk
+        np.savez('{}/scene_Accuracy_DOY_bin_{:02d}.npz'.format(result_path, DOY_bin), MCM_accuracy=MCM_accuracy, num_samples=num_samples)
+
+        # return MCM_accuracy
+
+if __name__ == '__main__':
+
+    import h5py
+    import os
+    import numpy as np
+    tables.file._open_files.close_all()
+    import mpi4py.MPI as MPI
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    for r in range(size):
+        if rank==r:
+
+            home = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/LA_PTA_MODIS_Data/try2_database'
+            DOY_bin = r
+            conf_matx_path = '{}/conf_matx_scene_all_DOY/conf_matx_scene_DOY_bin_{:02d}.HDF5'.format(home, DOY_bin)
+            result_path = '{}/{}'.format(home, 'scene_accuracy')
+            scene_conf_matx(conf_matx_path, DOY_bin, result_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #
---
+++ test_thresholds/plt_scene_accuracy_timeSeries.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+# 'scene_Accuracy_DOY_bin_{:02d}'.format(DOY_bin)
+
+home = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/LA_PTA_MODIS_Data/try2_database/scene_accuracy'
+scene_accur_paths = np.sort(np.array(os.listdir(home)))
+scene_accurs = np.zeros((1000,1000,len(scene_accur_paths)))
+
+plt.style.use('dark_background')
+fig, ax=plt.subplots(figsize=(10,10))
+cmap = cm.get_cmap('plasma', 20)
+plt.rcParams['font.size'] = 16
+container = []
+
+# results_ATBD = np.load('./results_so_cool.npz')
+# #plot_results(results_ATBD['MCM_accuracy'], results_ATBD['num_samples'])
+# scene_Accuracy_DOY_bin_{:02d}.npz
+
+for i, scene_accur_path in enumerate(scene_accur_paths):
+    with np.load('{}/{}'.format(home, scene_accur_path)) as npz_scene_accur:
+        scene_accurs[:,:,i] = npz_scene_accur['MCM_accuracy']*100
+        image = ax.imshow(scene_accurs[:,:,i], cmap=cmap, vmin=0, vmax=100)
+        DOY = (int(scene_accur_path[-6:-4]) + 1)*8
+        title = ax.text(0.5,1.05,'Accuracy DOY {}/365\nValid previous 8 days'.format(DOY),
+                        size=plt.rcParams["axes.titlesize"],
+                        ha="center", transform=ax.transAxes, )
+
+        container.append([image, title])
+
+ax.set_yticks([])
+ax.set_xticks([])
+image.cmap.set_under('k')
+#cbar_ax = fig.add_axes([0.93, 0.15, 0.02, 0.7])
+cbar = plt.colorbar(image,fraction=0.046, pad=0.04)
+cbar.set_ticks(np.arange(0,105,5))
+cbar.set_ticklabels([str(x) for x in np.arange(0,105,5)])
+
+ani = animation.ArtistAnimation(fig, container, interval=700, blit=False,
+                                repeat=True)
+#ani.save('./dynamic_images.mp4')
+
+plt.show()
