61cf0449e491b01e27c14728beb83ebee8e6fdb4
vllgsbr2 <vllgsbr2@illinois.edu>
2020-06-07 17:40:27 -0500

    did not need loop in batch or mpi for parallel read/write

--- scripts/combine_grouped_files.py
+++ scripts/combine_grouped_files.py
@@ -3,7 +3,7 @@ import h5py
 import numpy as np
 import sys
 
-def group_bins(home, group_dir, common_file, DOY_bin, rank):
+def group_bins(home, group_dir, common_file, DOY_bin):
     '''
     for easy parallelization and file writting over head, the bins were
     written into a file according to the processor that analyzed it. So
@@ -30,7 +30,8 @@ def group_bins(home, group_dir, common_file, DOY_bin, rank):
     group_dict = {}
 
     for i in range(46):
-        group =  home + '{}/grouped_data_DOY_{:03d}_to_{:03d}_bin_{:02d}_rank_{:02d}.hdf5'.format(group_dir, DOY_start, DOY_end, DOY_bin, i)
+        group =  home + '{}/grouped_data_DOY_{:03d}_to_{:03d}_bin_{:02d}_rank_{:02d}.hdf5'\
+                        .format(group_dir, DOY_start, DOY_end, DOY_bin, i)
         with h5py.File(group, 'r') as hf_group_:
             group_keys = list(hf_group_.keys())
 
@@ -40,25 +41,25 @@ def group_bins(home, group_dir, common_file, DOY_bin, rank):
                     group_dict.setdefault(key, [])
                     group_dict[key].append(data)
 
-    #keep trying to gain access until it is available
-    #once access is gained, it should write to file, and then exit while loop
-    being_accessed = True
-    while being_accessed:
-        try:
-            with h5py.File(home + 'grouped_obs_and_CMs/'  + common_file, 'a') as hf_group:
-                for key, val in group_dict.items():
-                    for arr in val:
-                        try:
-                            hf_group.create_dataset(key, data=np.array(arr), maxshape=(None,8))
+    # #keep trying to gain access until it is available
+    # #once access is gained, it should write to file, and then exit while loop
+    # being_accessed = True
+    # while being_accessed:
+    #     try:
+    with h5py.File(home + 'grouped_obs_and_CMs/'  + common_file, 'a') as hf_group:
+        for key, val in group_dict.items():
+            for arr in val:
+                try:
+                    hf_group.create_dataset(key, data=np.array(arr), maxshape=(None,8))
 
-                        except:
-                            group_shape = hf_group[key].shape[0]
-                            hf_group[key].resize(group_shape + np.array(arr).shape[0], axis=0)
-                            hf_group[key][group_shape:, :] = np.array(arr)
-            being_accessed = False
-        except Exception as e:
-            print(e)
-            being_accessed = True
+                except:
+                    group_shape = hf_group[key].shape[0]
+                    hf_group[key].resize(group_shape + np.array(arr).shape[0], axis=0)
+                    hf_group[key][group_shape:, :] = np.array(arr)
+        #     being_accessed = False
+        # except Exception as e:
+        #     print(e)
+        #     being_accessed = True
     #print('done')
 if __name__ == '__main__':
 
@@ -77,8 +78,8 @@ if __name__ == '__main__':
 
             home        = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/LA_PTA_MODIS_Data/try2_database/'
             group_dir   = 'group_60_cores'
-            DOY_bin     = int(sys.argv[1])
+            DOY_bin     = r#int(sys.argv[1])
             DOY_end     = (DOY_bin+1)*8
             DOY_start   =  DOY_end - 7
             common_file = 'grouped_obs_and_CM_{:03d}_to_{:03d}_bin_{:02d}.hdf5'.format(DOY_start, DOY_end, DOY_bin)
-            group_bins(home, group_dir, common_file, DOY_bin, rank)
+            group_bins(home, group_dir, common_file, DOY_bin)
