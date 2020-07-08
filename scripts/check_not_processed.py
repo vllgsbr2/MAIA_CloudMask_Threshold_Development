7eb6e8b24e8f1a300d0e4018e0d369ecd1e85735
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-08 15:53:09 -0500

    no try except

--- scripts/check_not_processed.py
+++ scripts/check_not_processed.py
@@ -7,16 +7,13 @@ files = os.listdir(home)
 
 #master dataframe to store all time stamps processed
 master_df = pd.DataFrame(data=None, columns=['time_stamps'], dtype=str)
-#master_df.columns = ['time_stamps']
 
 for f in files:
-    try:
-        temp_df = pd.read_csv(home+'/'+f, dtype=str)
-        temp_df.columns = ['num', 'time_stamp', 'phrase']
+    temp_df = pd.read_csv(home+'/'+f, dtype=str)
+    temp_df.columns = ['num', 'time_stamp', 'phrase']
 
-        master_df['time_stamps'] =  master_df['time_stamps'].append(temp_df['time_stamp'])
-    except:
-        pass
+    master_df['time_stamps'] =  master_df['time_stamps'].append(temp_df['time_stamp'])
+
 processed_files = list(master_df['time_stamps'])
 #remove space in front of time stamps
 processed_files = [x[1:] for x in processed_files]
@@ -40,43 +37,22 @@ PTA_file_path   = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/LA_PTA_MODI
 
 #grab files names for PTA
 filename_MOD_02 = np.array(os.listdir(PTA_file_path + '/MOD_02'))
-filename_MOD_03 = np.array(os.listdir(PTA_file_path + '/MOD_03'))
-filename_MOD_35 = np.array(os.listdir(PTA_file_path + '/MOD_35'))
 
 #sort files by time so we can access corresponding files without
 #searching in for loop
 filename_MOD_02 = np.sort(filename_MOD_02)
-filename_MOD_03 = np.sort(filename_MOD_03)
-filename_MOD_35 = np.sort(filename_MOD_35)
 
 #grab time stamp (YYYYDDD.HHMM) to name each group after the granule
 #it comes from
 time_stamps_downloaded = [x[10:22] for x in filename_MOD_02]
-# filename_MOD_03_timeStamp = [x[7:19]  for x in filename_MOD_03]
-# filename_MOD_35_timeStamp = [x[10:22] for x in filename_MOD_35]
+
 counter = 0
 for i in time_stamps_downloaded:
-
     if i in processed_files:
         print('{} not found #{:0>5d}'.format(i, counter))
         counter+=1
+
     else:
         check.write('{} not found'.format(i))
-        # print('{} not found #{:0>5d}'.format(i, counter))
-        # counter+=1
-    # found = False
-    # j=0
-    # print(counter)
-    # while not found:
-    #     if str(i)==str(processed_files[j][1:]):
-    #         found = True
-    #         check.write('{},{}\n'.format(i, found))
-    #         counter+=1
-    #         j+=1
-    #         if j==len(processed_files):
-    #
-    # if not found:
-    #     check.write('{},{}\n'.format(i, 0))
-# print(counter)
+
 check.close()
-# print(len(master_df))
