b1658e886cb557195fd978a7f4ecacf10813b5ce
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-15 12:54:10 -0500

    fixed directory syntax errors

--- scripts/MPI_create_dataset.py
+++ scripts/MPI_create_dataset.py
@@ -352,17 +352,17 @@ if __name__ == '__main__':
                                           filename_MOD_03[start:end]          ,\
                                           filename_MOD_35[start:end]          ,\
                                           filename_MOD_02_timeStamp[start:end]):
-                    print('{}\n{}\n{}\n{}\n{}'.format(i, MOD02, MOD03, MOD35, time_MOD02))
+                    # print('{}\n{}\n{}\n{}\n{}'.format(i, MOD02, MOD03, MOD35, time_MOD02))
 
-                    # try:
-                    build_data_base(MOD02, MOD03, MOD35, hf_path, hf,\
+                    try:
+                        build_data_base(MOD02, MOD03, MOD35, hf_path, hf,\
                                     time_MOD02, fieldname, target_lat,\
                                     target_lon)
 
-                    #     output.write('{:0>5d}, {}, {}'.format(i, time_MOD02, 'added to database\n'))
-                    #     print(i, time_MOD02, '\n')
-                    # except Exception as e:
-                    #
-                    #     output.write('{:0>5d}, {}, {}, {}'.format(i, time_MOD02, e, 'corrupt\n'))
-                    #     print(i, time_MOD02, 'corrupt\n')
+                        output.write('{:0>5d}, {}, {}'.format(i, time_MOD02, 'added to database\n'))
+                        print(i, time_MOD02, '\n')
+                    except Exception as e:
+
+                        output.write('{:0>5d}, {}, {}, {}'.format(i, time_MOD02, e, 'corrupt\n'))
+                        print(i, time_MOD02, 'corrupt\n')
                     i+=1
