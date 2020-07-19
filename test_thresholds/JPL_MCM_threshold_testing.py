af0d4db727e49c8c32b60c6fc377828dc30171cf
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-19 13:37:15 -0500

    dp

--- test_thresholds/JPL_MCM_threshold_testing.py
+++ test_thresholds/JPL_MCM_threshold_testing.py
@@ -848,6 +848,7 @@ def MCM_wrapper(test_data_JPL_path, Target_Area_X, threshold_filepath,\
     DOY,\
     Target_Area,\
     MOD03_sfctypes = get_JPL_data(test_data_JPL_path)
+    print(Target_Area, '*******************JPLMCM************')
 
     #define global shape for granule to process
     shape = rad_band_4.shape
