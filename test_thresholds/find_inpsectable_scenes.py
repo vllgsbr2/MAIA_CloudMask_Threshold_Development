9c2d79e0be2da7b8d866c7ec3f61bdc79607f759
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-24 16:38:23 -0500

    more mem and grsph rand scenes

--- test_thresholds/find_inpsectable_scenes.py
+++ test_thresholds/find_inpsectable_scenes.py
@@ -62,6 +62,71 @@ def choose_random_scenes():
 
         txt_good_scenes.write(rand_scenes_str)
 
+def graph_scenes(scenes_file):
+    '''
+    scenes_file {str} -- txt file where each row is time stamp YYYYDDD.HHMM
+                         i.e. 2020.048.1230 -> DOY 48, year 2020, 12:30 UTC
+    '''
+    import h5py
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from rgb_enhancement import *
+    import configparser
+
+    #grab relevant  scenes
+    with open(scenes_file, 'r') as txt_scenes:
+        scenes = []
+        for time_stamp in txt_scenes:
+            scenes.append(time_stamp)
+
+    #find output files
+    config_home_path = '/data/keeling/a/vllgsbr2/c/MAIA_thresh_dev/MAIA_CloudMask_Threshold_Development'
+    config           = configparser.ConfigParser()
+    config.read(config_home_path+'/test_config.txt')
+
+    PTA          = config['current PTA']['PTA']
+    PTA_path     = config['PTAs'][PTA]
+
+    #grab MCM and RGB from MCM Output files
+    output_home      = '{}/{}/'.format(PTA_path, config['supporting directories']['MCM_Output'])
+    output_dir       = os.listdir(output_home)
+    #use only scenes from scene file
+    output_dir       = [x for x in output_dir if x in scenes]
+    output_filepaths = [output_home + x + '/MCM_Output.h5' for x in output_dir]
+    time_stamps      = output_dir
+
+    # #grab MOD35 from here
+    # data_home          = '{}/{}/'.format(PTA_path, config['supporting directories']['Database'])
+    # database_files     = os.listdir(data_home)
+    # database_filepaths = [data_home + x for x in database_files]
+
+    for MCM_Output, time_stamp in zip(output_filepaths, time_stamps):
+
+        #get RGB and MCM
+        with h5py.File(MCM_Output, 'r') as hf_MCM_Output:
+            R_band_4 = hf_MCM_Output['Reflectance/band_04']
+            R_band_5 = hf_MCM_Output['Reflectance/band_05']
+            R_band_6 = hf_MCM_Output['Reflectance/band_06']
+
+            MCM = hf_MCM_Output['cloud_mask_output/final_cloud_mask']
+
+        #construct and enhance RGB
+        RGB            = np.dstack((R_band_6, R_band_5, R_band_4))
+        RGB[RGB==-999] = 0
+        RGB            = get_enhanced_RGB(RGB)
+
+        #plot RGB enhanced against MCM binary
+        f, ax = plt.subplots(ncols=2)
+
+        ax[0].imshow(MCM)
+        ax[1].imshow(RGB)
+
+        ax[0].set_title('MCM ' + time_stamp)
+        ax[1].set_title('RGB ' + time_stamp)
+
+        for a in ax:
+            a.set_xticks([])
+            a.set_yticks([])
 
 
 if __name__ == '__main__':
