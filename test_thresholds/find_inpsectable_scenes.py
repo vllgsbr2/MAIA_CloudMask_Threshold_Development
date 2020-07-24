78c0d65bbe80f6cea05a4098436ec30cbf8610ef
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-24 01:08:43 -0500

    dp

--- test_thresholds/find_inpsectable_scenes.py
+++ test_thresholds/find_inpsectable_scenes.py
@@ -46,6 +46,7 @@ def choose_random_scenes():
 
     scenes_2_inspect = []
     df_scenes   = pd.read_csv('./scenes_2_inspecting.txt', header=0, delimiter=' , ')
+    print(df_scenes.columns)
     time_stamps = df_scenes['time_stamp YYYYDDD.HHMM'].to_numpy()
 
     low, high = 0, time_stamps.shape[0]-1
