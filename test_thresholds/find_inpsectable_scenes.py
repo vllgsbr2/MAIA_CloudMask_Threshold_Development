3998f8e0db9dc42f7a3e091310c54b159751aaad
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-24 00:54:12 -0500

    choose random scenes

--- test_thresholds/find_inpsectable_scenes.py
+++ test_thresholds/find_inpsectable_scenes.py
@@ -15,7 +15,7 @@ def find_good_scenes():
     database_files     = os.listdir(data_home)
     database_filepaths = [data_home + x for x in database_files]
 
-    with open('./scenes_worth_inspecting.txt', 'w') as txt_good_scenes:
+    with open('./scenes_2_inspecting.txt', 'w') as txt_good_scenes:
         txt_good_scenes.write('time_stamp YYYYDDD.HHMM, % valid pixels\n')
         for db_file in database_filepaths:
             print(db_file[-30:])
@@ -40,5 +40,29 @@ def find_good_scenes():
                 txt_good_scenes.write(metadata)
             print(count_good_scenes)
 
+def choose_random_scenes():
+    import pandas as pd
+    import numpy as np
+
+    scenes_2_inspect = []
+    df_scenes   = pd.read_csv('./scenes_2_inspecting.txt', header=0, delimiter=' , ')
+    time_stamps = df_scenes['time_stamps'].to_numpy()
+
+    low, high = 0, time_stamps.shape[0]-1
+    size = 100
+    random_scene_idx = np.random.randint(low=low, high=high, size=size)
+    rand_scenes = time_stamps[random_scene_idx]
+    rand_scenes_str = ''
+
+    with open('./scenes_worth_inspecting.txt', 'w') as txt_good_scenes:
+        for scene in rand_scenes:
+            rand_scenes_str += '{}/n'.format(scene)
+
+
+        txt_good_scenes.write(rand_scenes_str)
+
+
+
 if __name__ == '__main__':
     find_good_scenes()
+    choose_random_scenes()
