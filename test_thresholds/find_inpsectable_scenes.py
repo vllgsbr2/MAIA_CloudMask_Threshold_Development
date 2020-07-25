cfae7834355599925f0ed694c1ba70c30960e8db
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-25 00:58:57 -0500

    d

--- test_thresholds/find_inpsectable_scenes.py
+++ test_thresholds/find_inpsectable_scenes.py
@@ -78,7 +78,7 @@ def graph_scenes(scenes_file):
 
     #grab relevant  scenes
     df_scenes   = pd.read_csv('./scenes_worth_inspecting.txt', header=None, dtype=str)
-    scenes = df_scenes.values
+    scenes = df_scenes.values[:,0]
     print(scenes)
 
     # with open(scenes_file, 'r') as txt_scenes:
