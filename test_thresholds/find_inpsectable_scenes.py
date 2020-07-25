cc27dd2b8b507d3088408b9f3af1ba64f9cd34fa
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-25 01:08:42 -0500

    d

--- test_thresholds/find_inpsectable_scenes.py
+++ test_thresholds/find_inpsectable_scenes.py
@@ -143,6 +143,6 @@ def graph_scenes(scenes_file):
 
 if __name__ == '__main__':
     # find_good_scenes()
-    choose_random_scenes()
+    # choose_random_scenes()
     scenes_file = './scenes_worth_inspecting.txt'
     graph_scenes(scenes_file)
