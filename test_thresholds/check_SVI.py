3289d788fcc800faee5aac8d20a922992fdcb6a3
vllgsbr2 <vllgsbr2@illinois.edu>
2020-07-27 14:37:49 -0500

    more precision on percent

--- test_thresholds/check_SVI.py
+++ test_thresholds/check_SVI.py
@@ -33,4 +33,4 @@ for thresh_file in thresh_files:
             num_negative_SVI += len(SVI[(SVI<0) & (SVI != fill_val)])
             num_positive_SVI += len(SVI[SVI>=0])
 
-    print('%neg {:1.2f}, neg {:05d}, pos {:05d}'.format(num_negative_SVI/num_positive_SVI, num_negative_SVI, num_positive_SVI))
+    print('%neg {:1.6f}, neg {:05d}, pos {:05d}'.format(num_negative_SVI/num_positive_SVI, num_negative_SVI, num_positive_SVI))
