d8adc3db38e976e2dc908f1c45122313f1c2e007
vllgsbr2 <vllgsbr2@illinois.edu>
2020-08-04 17:09:54 -0500

    plot box plot against sfc ID now

--- test_thresholds/analyze_thresh.py
+++ test_thresholds/analyze_thresh.py
@@ -71,10 +71,11 @@ def check_neg_SVI_grouped():
             # print('%neg {:1.6f}, num neg {:05d}, num pos {:05d}, neg SVIs {}'.format(num_negative_SVI/num_positive_SVI, num_negative_SVI, num_positive_SVI, neg_SVI))
             print('%neg {:1.6f}, num neg {:05d}, num pos {:05d}'.format(num_negative_SVI/num_positive_SVI, num_negative_SVI, num_positive_SVI))
 
-def check_thresh(which_thresh, flatten_or_nah=True):
+def check_thresh(which_thresh, flatten_or_nah=True, by_SFC_ID_or_nah=True):
     '''
     which_thresh {str} -- choose from WI,NDVI,NDSI,VIS_Ref,NIR_Ref,SVI,Cirrus
     flatten_or_nah {bool} -- True filter values and flatten; False replace w/nan
+    by_SFC_ID_or_nah {bool} -- True shows thresh applied by sfcID; False all thresh
     '''
 
     thresh_dict = {'WI':0, 'NDVI':1, 'NDSI':2, 'VIS_Ref':3, 'NIR_Ref':4,\
@@ -97,25 +98,27 @@ def check_thresh(which_thresh, flatten_or_nah=True):
 
             thresh = hf_thresh[thresh_path][()]#.flatten()
 
-            #axis 3 is sceneID. Store thresh when sceneID is valid for thresh
-            if thresh_dict[which_thresh] == 0:
-                # WI no glint or snow
-                thresh = thresh[:,:,:,0:13]
-            elif thresh_dict[which_thresh] == 1:
-                #NDVI no snow
-                thresh = thresh[:,:,:,0:14]
-            elif thresh_dict[which_thresh] == 2:
-                #NDSI only snow
-                thresh = thresh[:,:,:,14]
-            elif thresh_dict[which_thresh] == 3:
-                #VIS only land
-                thresh = thresh[:,:,:,0:12]
-            elif thresh_dict[which_thresh] == 4:
-                #NIR only non glint water
-                thresh = thresh[:,:,:,12]
-            else:
-                #everything
-                pass
+            if by_SFC_ID_or_nah:
+                #axis 3 is sceneID. Store thresh when sceneID is valid for thresh
+                if thresh_dict[which_thresh] == 0:
+                    # WI no glint or snow
+                    thresh = thresh[:,:,:,0:13]
+                elif thresh_dict[which_thresh] == 1:
+                    #NDVI no snow
+                    thresh = thresh[:,:,:,0:14]
+                elif thresh_dict[which_thresh] == 2:
+                    #NDSI only snow
+                    thresh = thresh[:,:,:,14]
+                elif thresh_dict[which_thresh] == 3:
+                    #VIS only land
+                    thresh = thresh[:,:,:,0:12]
+                elif thresh_dict[which_thresh] == 4:
+                    #NIR only non glint water
+                    thresh = thresh[:,:,:,12]
+                else:
+                    #everything
+                    pass
+
             # only take positve/non- fill_val thresholds from
             # WI/VIS/NIR/SVI/Cirrus
             if thresh_dict[which_thresh] >= 3 or \
@@ -123,15 +126,12 @@ def check_thresh(which_thresh, flatten_or_nah=True):
 
                if flatten_or_nah:
                    thresh = thresh[(thresh >= 0) & (thresh != fill_val)]
-               # else:
-                   # thresh[(thresh >= 0) & (thresh != fill_val)] = fill_val#np.nan
 
             #take out fill val from NDVI/NDSI
             else:
                 if flatten_or_nah:
                     thresh = thresh[thresh != fill_val]
-                # else:
-                    # thresh[thresh != fill_val] = fill_val
+
 
     return thresh
 
@@ -230,12 +230,60 @@ def plot_thresh_vs_VZA():
 
     plt.show()
 
+def plot_thresh_vs_sfcID():
+    import matplotlib.pyplot as plt
+    #make histograms of thresholds
+    thresh_dict = {'WI':0, 'NDVI':1, 'NDSI':2, 'VIS_Ref':3, 'NIR_Ref':4,\
+                   'SVI':5, 'Cirrus':6}
+    fill_val = -999
+
+    thresholds = []
+    for i, obs in enumerate(thresh_dict):
+        thresholds.append(check_thresh(obs, flatten_or_nah=False, by_SFC_ID_or_nah=True))
+
+    range_ndxi     = (-1.,1.)
+    range_other    = (0. ,1.5)
+    num_bins_ndxi  = 100
+    num_bins_other = int(num_bins_ndxi * (range_other[1] - range_other[0]) / \
+                                         (range_ndxi[1]  - range_ndxi[0] ))
+
+    f, ax = plt.subplots(ncols=4, nrows=2)
+
+    for i, (a, obs) in enumerate(zip(ax.flat, thresh_dict)):
+        #make a deep copy because to not modify it
+        thresh_obs_i  = np.copy(thresholds[i])
+        #reorder threshold dims so sfcID is first
+        thresh_obs_i  = np.moveaxis(thresh_obs_i, 3, 0)
+        thresh_shape  = thresh_obs_i.shape
+
+        #reshape so VZA is axis 0 and the other axis is everything else flattened
+        shape_cosSZA_x_RAZ_x_VZA = np.prod(thresh_shape[1:])
+        thresh_obs_i  = thresh_obs_i.reshape(thresh_shape[0], shape_cosSZA_x_RAZ_x_VZA)
+
+        # #normalize
+        # thresh_obs_i  = thresh_obs_i/thresh_obs_i.max()
+
+        #eliminate fill vals while keeping a vector for each VZA bin
+        boxplot_thresh_obs_i = []
+        for thresh_sfcID_x_i in thresh_obs_i:
+            boxplot_thresh_obs_i.append(thresh_vza_x_i[thresh_sfcID_x_i != fill_val])
+
+        a.boxplot(boxplot_thresh_obs_i, notch=False, sym='')
+        a.set_title(obs)
+
+        #only 7 obs so lets turn 8th axis off
+        if i==7:
+            a.axis('off')
+
+    plt.show()
+
 if __name__ == '__main__':
 
     # check_neg_SVI_thresh()
     # check_neg_SVI_grouped()
     # plot_thresh_hist()
-    plot_thresh_vs_VZA()
+    # plot_thresh_vs_VZA()
+    plot_thresh_vs_sfcID()
 
 
 
