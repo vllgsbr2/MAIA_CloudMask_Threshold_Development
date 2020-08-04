4ccefdcf0e914729b166b4b26d5bb6c110519b07
vllgsbr2 <vllgsbr2@illinois.edu>
2020-08-03 19:53:45 -0500

    d

--- test_thresholds/check_SVI.py
+++ test_thresholds/check_SVI.py
@@ -202,7 +202,7 @@ def plot_thresh_vs_VZA():
         print(thresholds[i].shape)
         thresh_obs_i  = np.moveaxis(thresh_obs_i, 1, 0)
         thresh_shape  = thresholds[i].shape
-        print(thresh_shape)
+        print(thresholds[i].shape)
         #If test is applied on only one surface type then only 3 dims
         if len(thresh_shape) == 4:
             #reshape so VZA is axis 0 and the other axis is everything else flattened
