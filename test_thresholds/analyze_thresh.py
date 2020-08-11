5f3f335eedee72ea22900ca7a641a5c1bb5455d6
vllgsbr2 <vllgsbr2@illinois.edu>
2020-08-11 14:17:13 -0500

    d

--- test_thresholds/analyze_thresh.py
+++ test_thresholds/analyze_thresh.py
@@ -352,8 +352,8 @@ if __name__ == '__main__':
     # check_neg_SVI_grouped()
     # plot_thresh_hist()
     # plot_thresh_vs_VZA()
-    # plot_thresh_vs_sfcID()
-    check_sunglint_thresh()
+    plot_thresh_vs_sfcID()
+    # check_sunglint_thresh()
 
 
 
