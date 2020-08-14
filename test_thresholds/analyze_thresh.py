6f45d5b6b6ac8e7e9b8c66997b4198ad304eb8f9
vllgsbr2 <vllgsbr2@illinois.edu>
2020-08-14 15:57:18 -0500

    back to cirrus

--- test_thresholds/analyze_thresh.py
+++ test_thresholds/analyze_thresh.py
@@ -489,7 +489,7 @@ if __name__ == '__main__':
     # check_sunglint_thresh()
     # check_sunglint_flag_in_database()
     # check_sunglint_flag_in_grouped_cm_and_obs()
-    make_obs_hist_by_group('VIS_Ref')
+    make_obs_hist_by_group('Cirrus')
 
 
 
