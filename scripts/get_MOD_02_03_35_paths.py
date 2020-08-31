52816c20594fbbecf09b1f465994aa5398f465fa
vllgsbr2 <vllgsbr2@illinois.edu>
2020-08-31 17:00:02 -0500

    d

--- scripts/get_MOD_02_03_35_paths.py
+++ scripts/get_MOD_02_03_35_paths.py
@@ -11,14 +11,12 @@ def get_MODIS_file_paths(MOD02_txt, MOD03_txt, MOD35_txt):
 
     if len(MOD02_paths) == len(MOD03_paths) and len(MOD02_paths) == len(MOD35_paths):
         for count, (i,j,k) in enumerate(zip(MOD02_paths, MOD03_paths, MOD35_paths)):
-            print(i)
-            print(os.path.isfile(i))
-            # if not os.path.isfile(i):
-            # print(count, i, 'is DNE')
-            # if not os.path.isfile(j):
-            #     print(count, j, 'is DNE')
-            # if not os.path.isfile(k):
-            #     print(count, k, 'is DNE')
+            if not os.path.isfile(i[-44:]):
+            print(count, i, 'is DNE')
+            if not os.path.isfile(j[-34:]):
+                print(count, j, 'is DNE')
+            if not os.path.isfile(k[-44:]):
+                print(count, k, 'is DNE')
             # print(count)
 
         return MOD02_paths, MOD03_paths, MOD35_paths
