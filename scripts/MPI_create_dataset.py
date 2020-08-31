41b9085be4f243f9a7feeb62ea8ae265e2b7199e
vllgsbr2 <vllgsbr2@illinois.edu>
2020-08-31 17:23:00 -0500

    d

--- scripts/MPI_create_dataset.py
+++ scripts/MPI_create_dataset.py
@@ -380,8 +380,8 @@ if __name__ == '__main__':
 
                     # try:
                     build_data_base(MOD02, MOD03, MOD35, hf_path, hf,\
-                    #                 time_MOD02, fieldname, target_lat,\
-                    #                 target_lon)
+                                     time_MOD02, fieldname, target_lat,\
+                                     target_lon)
                     #
                     #     output.write('{:0>5d}, {}, {}'.format(i, time_MOD02, 'added to database\n'))
                     print(i, time_MOD02, '\n')
