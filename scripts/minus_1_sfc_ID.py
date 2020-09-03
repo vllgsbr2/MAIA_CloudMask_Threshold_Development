9fc97de20f1879ab284647021af0a5814ac55452
vllgsbr2 <vllgsbr2@illinois.edu>
2020-09-03 18:10:32 -0500

    adapted to run old SID for performance test

--- scripts/minus_1_sfc_ID.py
+++ scripts/minus_1_sfc_ID.py
@@ -4,6 +4,47 @@ from netCDF4 import Dataset
 import os
 import sys
 
+def make_SID_MCM_rdy_old(home_og, home):
+    '''
+    home_og {str} -- path to original surface ID files for 1 PTA
+    home {str} -- path to directory to put modified surface ID files for 1 PTA
+    '''
+
+    #grab OG surface IDs
+    sfc_ID_paths = np.sort(np.array(os.listdir(home_og)))
+    sfc_ID_paths = [x for x in sfc_ID_paths if x[:12] == 'surfaceID_Lo']
+    sfc_IDs = np.zeros((400,300,len(sfc_ID_paths)))
+
+    for i, sfc_ID_path in enumerate(sfc_ID_paths):
+        with Dataset(home_og + sfc_ID_path, 'r') as nc_sfc_ID:
+            sfc_IDs[:,:,i] = nc_sfc_ID.variables['surface_ID'][:,:]
+            # print(sfc_IDs[:,:,i].max())
+
+    # 0  - Water
+    # 1  - darkest; Coastline; invalid retrievals
+    # 12 - Brightest
+
+    #modify the surface ID in memory
+    sfc_IDs_mod = np.copy(sfc_IDs) - 1
+    #set sfcID -1 to 12
+    sfc_IDs_mod[sfc_IDs ==-1]  = 12
+
+    #copy files into new destination
+    os.system('cp {}/surfaceID* {}/'.format(home_og, home))
+
+    #edit copied surface IDs files with new surface ID
+    sfc_ID_paths = np.sort(np.array(os.listdir(home)))
+    sfc_ID_paths = [x for x in sfc_ID_paths if x[:12] == 'surfaceID_Lo']
+
+    for i, sfc_ID_path in enumerate(sfc_ID_paths):
+        with Dataset(home + sfc_ID_path, 'r+') as nc_sfc_ID:
+            nc_sfc_ID.variables['surface_ID'][:,:] = sfc_IDs_mod[:,:,i]
+
+    sfc_IDs_read_mod = np.zeros((400,300,len(sfc_ID_paths)))
+    for i, sfc_ID_path in enumerate(sfc_ID_paths):
+        with Dataset(home + sfc_ID_path, 'r') as nc_sfc_ID:
+            sfc_IDs_read_mod[:,:,i] = nc_sfc_ID.variables['surface_ID'][:,:]
+
 
 def make_SID_MCM_rdy(home_og, home):
     '''
@@ -61,12 +102,12 @@ def make_SID_MCM_rdy(home_og, home):
     for i, sfc_ID_path in enumerate(sfc_ID_paths):
         with Dataset(home + sfc_ID_path, 'r') as nc_sfc_ID:
             sfc_IDs_read_mod[:,:,i] = nc_sfc_ID.variables['surface_ID'][:,:]
-            
-    im=plt.imshow(sfc_IDs_read_mod[:,:,0], cmap='jet', vmin=0, vmax=11)
-    im.cmap.set_over('k')
-    im.cmap.set_under('pink')
-    plt.show()
-    sys.exit()
+
+    # im=plt.imshow(sfc_IDs_read_mod[:,:,0], cmap='jet', vmin=0, vmax=11)
+    # im.cmap.set_over('k')
+    # im.cmap.set_under('pink')
+    # plt.show()
+    # sys.exit()
 
 
 if __name__ == '__main__':
@@ -79,6 +120,10 @@ if __name__ == '__main__':
     PTA      = config['current PTA']['PTA']
     # PTA_path = config['PTAs'][PTA]
 
+    # home        = '/data/gdi/c/gzhao1/MCM-surfaceID/SfcID/{}/'.format(PTA)
+    # destination = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/PTAs/{}/Surface_IDs/'.format(PTA)
+    # make_SID_MCM_rdy(home, destination)
+
     home        = '/data/gdi/c/gzhao1/MCM-surfaceID/SfcID/{}/'.format(PTA)
-    destination = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/PTAs/{}/Surface_IDs/'.format(PTA)
-    make_SID_MCM_rdy(home, destination)
+    destination = '/data/gdi/c/gzhao1/MCM-surfaceID/SfcID/version1/'
+    make_SID_MCM_rdy_old(home, destination)
