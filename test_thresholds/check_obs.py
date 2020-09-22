b77d7be6a29ec4704f8ccd0a7c89c54fafc08d57
vllgsbr2 <vllgsbr2@illinois.edu>
2020-09-22 12:44:07 -0500

    d

--- test_thresholds/check_obs.py
+++ test_thresholds/check_obs.py
@@ -23,24 +23,26 @@ for r in range(size):
         obs_files     = [obs_filepaths + x for x in os.listdir(obs_filepaths)]
         obs_f = obs_files[r]
 
-        Database_filepaths = '{}/{}/'.format(PTA_path, config['supporting directories']['Database'])
-        Database_files     = [Database_filepaths + x for x in os.listdir(Database_filepaths)]
-        Database_f = Database_files[r]
+        input_home = '{}/{}'.format(PTA_path, config['supporting directories']['MCM_Input'])
+
+        # Database_filepaths = '{}/{}/'.format(PTA_path, config['supporting directories']['Database'])
+        # Database_files     = [Database_filepaths + x for x in os.listdir(Database_filepaths)]
+        # Database_f = Database_files[r]
+
 
         high_ref_samples = []
         time_stamps_high_ref = []
         # for obs_f in obs_files:
 
-        with h5py.File(obs_f, 'r') as hf_observables,\
-             h5py.File(Database_f, 'r') as hf_database:
+        with h5py.File(obs_f, 'r') as hf_observables:
             obs_keys = list(hf_observables.keys())
-            database_keys = list(hf_database.keys())
-            print(obs_keys)
-            print(database_keys)
+
             for time_stamp in obs_keys:
+                Database_f = '{}/test_JPL_data_{}.h5'.format(input_home, time_stamp)
+                with h5py.File(Database_f, 'r') as hf_database:
                 cirrus_Ref = hf_observables[time_stamp + '/cirrus'][()]
                 vis_Ref    = hf_observables[time_stamp + '/visRef'][()]
-                cloud_Mask = hf_database[time_stamp + '/cloud_mask/Unobstructed_FOV_Quality_Flag']
+                cloud_Mask = hf_database['MOD35_cloud_mask'][()]
 
                 high_cirrus_obs_idx = np.where((cirrus_Ref > 0.4) & (cloud_Mask != 0))
                 cirrus_Ref_          = cirrus_Ref[high_cirrus_obs_idx]
