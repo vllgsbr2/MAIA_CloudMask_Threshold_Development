d9853c69b016ce8993eb8e1e566858bc7b3cbab9
vllgsbr2 <vllgsbr2@illinois.edu>
2020-09-22 12:32:56 -0500

    d

--- test_thresholds/check_obs.py
+++ test_thresholds/check_obs.py
@@ -21,30 +21,37 @@ for r in range(size):
         #read hdf5 file to store observables
         obs_filepaths = '{}/{}/'.format(PTA_path, config['supporting directories']['obs'])
         obs_files     = [obs_filepaths + x for x in os.listdir(obs_filepaths)]
+        obs_f = obs_files[r]
+
+        Database_filepaths = '{}/{}/'.format(PTA_path, config['supporting directories']['Database'])
+        Database_files     = [Database_filepaths + x for x in os.listdir(Database_filepaths)]
+        Database_f = Database_files[r]
 
         high_ref_samples = []
         time_stamps_high_ref = []
         # for obs_f in obs_files:
-        obs_f = obs_files[r]
-        with h5py.File(obs_f, 'r') as hf_observables:
+
+        with h5py.File(obs_f, 'r') as hf_observables,\
+             h5py.File(Database_f, 'r') as hf_database:
             obs_keys = list(hf_observables.keys())
             for time_stamp in obs_keys:
                 cirrus_Ref = hf_observables[time_stamp + '/cirrus'][()]
                 vis_Ref    = hf_observables[time_stamp + '/visRef'][()]
+                cloud_Mask = hf_database[time_stamp + '/cloud_mask/Unobstructed_FOV_Quality_Flag']
 
-                high_cirrus_obs_idx = np.where(cirrus_Ref > 0.4)
+                high_cirrus_obs_idx = np.where((cirrus_Ref > 0.4) & (cloud_Mask != 0))
                 cirrus_Ref_          = cirrus_Ref[high_cirrus_obs_idx]
 
                 num_pix_high        = high_cirrus_obs_idx[0].shape[0]
                 if num_pix_high > 0:
-                    high_ref_samples.append(cirrus_Ref_)
-                    time_stamps_high_ref.append(time_stamp)
+                    # high_ref_samples.append(cirrus_Ref_)
+                    # time_stamps_high_ref.append(time_stamp)
                     # print(high_ref_samples)
                     print(time_stamp, num_pix_high)
-                if time_stamp == '2010192.1825':
-                    # plt.imshow(cirrus_Ref, cmap='bone')
-                    plt.imshow(vis_Ref, cmap='bone')
-                    plt.colorbar()
-                    # plt.title(time_stamp+'1.38 microns BRF')
-                    plt.title(time_stamp+'0.65 microns BRF')
-                    plt.show()
+                # if time_stamp == '2010192.1825':
+                #     # plt.imshow(cirrus_Ref, cmap='bone')
+                #     plt.imshow(vis_Ref, cmap='bone')
+                #     plt.colorbar()
+                #     # plt.title(time_stamp+'1.38 microns BRF')
+                #     plt.title(time_stamp+'0.65 microns BRF')
+                #     plt.show()
