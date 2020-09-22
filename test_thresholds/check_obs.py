274f6988f94600fcdc0c5eac423794c127ead705
vllgsbr2 <vllgsbr2@illinois.edu>
2020-09-22 12:48:41 -0500

    d

--- test_thresholds/check_obs.py
+++ test_thresholds/check_obs.py
@@ -55,7 +55,7 @@ for r in range(size):
                         print(time_stamp, num_pix_high)
                     if time_stamp == '2010197.1845':
                         print(high_cirrus_obs_idx)
-                        plt.imshow(cirrus_Ref, cmap='bone')
+                        plt.imshow(cirrus_Ref, cmap='jet', vmin=0, vmax = 0.4)
                         plt.title(time_stamp+'1.38 microns BRF')
                         # plt.title(time_stamp+'0.65 microns BRF')
                         # plt.imshow(vis_Ref, cmap='bone')
