a343a9fb462e94924cab95162390f4bc9ff4a65c
vllgsbr2 <vllgsbr2@illinois.edu>
2020-09-23 15:15:24 -0500

    d

--- test_thresholds/analyze_thresh.py
+++ test_thresholds/analyze_thresh.py
@@ -93,7 +93,7 @@ def check_thresh(which_thresh, flatten_or_nah=True, by_SFC_ID_or_nah=True):
             thresh_path = '{}/{}/{}'.format('TA_bin_00', DOY,\
                                             obs[thresh_dict[which_thresh]])
             current_thresh = hf_thresh[thresh_path][()]
-            large_cirrus_thresh_idx = np.where(current_thresh > 0.4)
+            large_cirrus_thresh_idx = np.where(current_thresh > 0.7)
             for i, j, k, m in zip(large_cirrus_thresh_idx[0], large_cirrus_thresh_idx[1], large_cirrus_thresh_idx[2], large_cirrus_thresh_idx[3]):
 
                 debug_print_str = 'thresh {:1.4f} cosSZA {:02d} VZA {:02d} RAA {:02d} SID {:02d} DOY {:02d}'.format(current_thresh[i,j,k,m], i, j, k, m, DOY_)
