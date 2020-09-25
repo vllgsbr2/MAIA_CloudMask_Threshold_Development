1f68919959976494bdf4c5e51924591d3ec9a95f
vllgsbr2 <vllgsbr2@illinois.edu>
2020-09-25 14:22:24 -0500

    d

--- test_thresholds/check_obs.py
+++ test_thresholds/check_obs.py
@@ -57,8 +57,8 @@ for r in range(size):
                     if time_stamp == '2010197.1845':
                         # print(high_cirrus_obs_idx)
                         #
-                        # plt.imshow(vis_Ref, cmap='binary_r')
-                        plt.imshow(cirrus_Ref, cmap='binary_r', vmin=0)
+                        plt.imshow(vis_Ref, cmap='binary_r')
+                        # plt.imshow(cirrus_Ref, cmap='binary_r', vmin=0)
                         plt.yticks([])
                         plt.xticks([])
                         plt.colorbar()
