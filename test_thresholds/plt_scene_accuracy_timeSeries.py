2351c8fe3e64c347ed9c19e1fac7c0afcb0b2901
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-03 16:22:36 -0500

    d

--- test_thresholds/plt_scene_accuracy_timeSeries.py
+++ test_thresholds/plt_scene_accuracy_timeSeries.py
@@ -56,6 +56,9 @@ for i in range(46):
     s_list.append(np.mean(s_temp))
 
 plt.bar(np.arange(8,376,8), s_list)
+plt.xticks(np.arange(8,376,8))
+plt.xlabel('Julian Day of Year')
+plt.yabel('% Accuracy')
 
 
 plt.show()
