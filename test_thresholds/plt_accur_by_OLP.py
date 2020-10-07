a7450bd3ee147dd96a4b65affb8e9c7b8d54fb55
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-07 16:03:49 -0500

    d

--- test_thresholds/plt_accur_by_OLP.py
+++ test_thresholds/plt_accur_by_OLP.py
@@ -69,6 +69,7 @@ color = 'tab:pink'
 ax.set_xlabel('Surface Type')
 ax.set_ylabel('% Accuracy', color=color)
 ax.set_ylim(75, 100)
+ax.set_xticks(x)
 ax.scatter(x, s_list, color=color)
 ax.plot(x, s_list, color=color)
 ax.tick_params(axis='y', labelcolor=color)
