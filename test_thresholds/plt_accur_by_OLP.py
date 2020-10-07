b4bdf782c1e789d8beb273499ab582f39975229f
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-07 15:32:20 -0500

    d

--- test_thresholds/plt_accur_by_OLP.py
+++ test_thresholds/plt_accur_by_OLP.py
@@ -75,7 +75,7 @@ ax1 = ax.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:cyan'
 ax1.set_ylabel('number of samples', color=color)  # we already handled the x-label with ax1
-ax1.bar(x, num_samples_list, color=color)
+ax1.plot(x, num_samples_list, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 
 plt.show()
