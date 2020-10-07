7ff99bee1365304626540a2e056c2328c0045432
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-07 16:37:38 -0500

    d

--- test_thresholds/plt_SVG_polar_accur.py
+++ test_thresholds/plt_SVG_polar_accur.py
@@ -17,29 +17,52 @@ scene_accur_path = scene_accur_home + '/' + 'group_ID_accuracy.h5'
 filepath = scene_accur_path
 
 
-s_list = np.zeros((15,10,12))
-num_samples_list=np.zeros((15,10,12))
+# s_list = np.zeros((15,10,12))
+# num_samples_list=np.zeros((15,10,12))
+#
+# with h5py.File(filepath, 'r') as hf:
+#     bins = list(hf.keys())
+#
+#     for VZA in range(15):
+#         for SZA in range(10):
+#             for RAA in range(12):
+#                 #place to store for each bin
+#                 accuracy =[]
+#                 num_samples=[]
+#                 #find group names of bin combo in for loop step
+#                 bin_IDs = [x for x in bins if x[:40]=='confusion_matrix_cosSZA_{:02d}_VZA_{:02d}_RAZ_{:02d}'.format(SZA,VZA,RAA)]
+#                 #cycle through all the unque SVG bins (DOY and SID may change ofcourse)
+#                 for i, bin_ID in enumerate(bin_IDs):
+#                     # print(SZA,VZA,RAA,bin_ID)
+#                     accuracy.append(hf[bin_ID+'/accuracy'][()])
+#                     # print(hf[bin_ID+'/accuracy'][()])
+#                     num_samples.append(hf[bin_ID+'/num_samples'][()])
+#                 s_temp = np.array(accuracy)
+#                 num_samples_temp = np.array(num_samples)
+#                 # s_temp = s_temp[s_temp>=0]
+#                 s_list[VZA,SZA,RAA] = np.nanmean(s_temp)*100
+#                 num_samples_list[VZA,SZA,RAA] = np.nansum(np.array(num_samples))
+# np.savez('./SVG_accur_data.npz', accuracy=s_list, num_samples=num_samples_list)
 
-with h5py.File(filepath, 'r') as hf:
-    bins = list(hf.keys())
 
-    for VZA in range(15):
-        for SZA in range(10):
-            for RAA in range(12):
-                #place to store for each bin
-                accuracy =[]
-                num_samples=[]
-                #find group names of bin combo in for loop step
-                bin_IDs = [x for x in bins if x[:40]=='confusion_matrix_cosSZA_{:02d}_VZA_{:02d}_RAZ_{:02d}'.format(SZA,VZA,RAA)]
-                #cycle through all the unque SVG bins (DOY and SID may change ofcourse)
-                for i, bin_ID in enumerate(bin_IDs):
-                    # print(SZA,VZA,RAA,bin_ID)
-                    accuracy.append(hf[bin_ID+'/accuracy'][()])
-                    # print(hf[bin_ID+'/accuracy'][()])
-                    num_samples.append(hf[bin_ID+'/num_samples'][()])
-                s_temp = np.array(accuracy)
-                num_samples_temp = np.array(num_samples)
-                # s_temp = s_temp[s_temp>=0]
-                s_list[VZA,SZA,RAA] = np.nanmean(s_temp)*100
-                num_samples_list[VZA,SZA,RAA] = np.nansum(np.array(num_samples))
-np.savez('./SVG_accur_data.npz', accuracy=s_list, num_samples=num_samples_list)
+#-- Generate Data -----------------------------------------
+# Using linspace so that the endpoint of 360 is included...
+azimuths = np.radians(np.linspace(0, 192, 12))
+zeniths = np.arange(0, 70, 10)
+
+r, theta = np.meshgrid(zeniths, azimuths)
+# values = np.random.random((azimuths.size, zeniths.size))
+
+data = np.load('./SVG_accur_data.npz')
+dataset_names = data.files
+
+accuracy_SVG = data[dataset_names[0]]
+num_smaples_SVG = data[dataset_names[1]]
+
+print(accuracy_SVG.size, num_smaples_SVG.size)
+
+# #-- Plot... ------------------------------------------------
+# fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
+# ax.contourf(theta, r, values)
+#
+# plt.show()
