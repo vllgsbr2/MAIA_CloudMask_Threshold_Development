3828fe2a60681c12d6ee5b31940f638e0647ccf9
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-15 15:28:11 -0500

    to figure out num clusters for SID

---
+++ test_thresholds/graph_SMB_files.py
@@ -0,0 +1,43 @@
+'''
+Author     : Javier Villegas Bravo
+Association: UIUC and NASA JPL
+Date       : 10/15/20
+
+PURPOSE:
+graph cross sections of SMB data to find how many k-means clusters we should
+choose for the surface type scheme
+'''
+import numpy as np
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+import os
+
+SMB_home = '/data/gdi/c/gzhao1/MCM-surfaceID/SMB/LosAngeles/'
+SMB_files = [SMB_home + x for x in os.listdir(SMB_home) if x[:3]=='max']
+
+SMB_file_x = SMB_files[0]
+
+with Dataset(SMB_file_x, 'r') as nc_SMB:
+    #holds max_BRF with dims (x, y, cos_sza, vza, raz) -> (400,300,10,15,12)
+    max_BRF = nc_SMB.varaibles['max_BRF'][:]
+    cos_sza = nc_SMB.varaibles['cos_sza'][:]
+    vza = nc_SMB.varaibles['vza'][:]
+    raz = nc_SMB.varaibles['raz'][:]
+
+max_BRF_flat = max_BRF.reshape((400*300,10,15,12))
+max_BRF_by_SVC = np.zeros((10,15,12))
+for i in cos_sza:
+    for j in vza:
+        for k in raz:
+            max_BRF_by_SVC[i,j,k] = max_BRF_flat[:,i,j,k].mean()
+
+max_BRF_by_SVC = max_BRF_by_SVC.reshape(10*15*12)
+max_BRF_by_SVC = np.sort(max_BRF_by_SVC)
+
+
+f, ax = plt.subplots(ncols=1,nrows=1)
+
+ax.plot(np.arange(10*15*12), max_BRF_by_SVC)
+ax.scatter(np.arange(10*15*12), max_BRF_by_SVC)
+ax.set_xlabel('Sun View Geometry Combinations 0-1799')
+ax.set_ylabel('Mean BRDF for SVGC')
