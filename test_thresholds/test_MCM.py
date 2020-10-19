d2afd797d544b7d23c96bdbf3b221e42c3ca9f01
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-19 11:43:56 -0500

    d

--- test_thresholds/test_MCM.py
+++ test_thresholds/test_MCM.py
@@ -27,6 +27,11 @@ home = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/LA_PTA_MODIS_Data/try2
 # test_scene = 'test_JPL_data_2019137.1825.h5' # great scene!! would like more water clouds
 # test_scene = 'test_JPL_data_2019096.1830.h5' #great scene no water clouds :(
 test_scene = 'test_JPL_data_2018365.1830.h5' #this is it!!
+
+#test some Guangyu scenes
+test_scene = 'test_JPL_data_2019285.1800.h5'
+# test_scene = 'test_JPL_data_2019276.1805.h5'
+
 # test_scene = 'test_JPL_data_2019089.1825.h5'
 DOY = int(test_scene[18:-8])
 DOY_bin = np.digitize(DOY, np.arange(8,376,8), right=True)
