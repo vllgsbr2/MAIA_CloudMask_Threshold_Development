13179b77916420ad9fc767aa64975071045a5243
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-28 20:22:53 -0500

    test new thresh

--- test_thresholds/test_MCM.py
+++ test_thresholds/test_MCM.py
@@ -42,7 +42,7 @@ DOY_bin = np.digitize(DOY, np.arange(8,376,8), right=True)
 DOY_end = (DOY_bin+1)*8
 DOY_start = DOY_end - 7
 
-thresh_file = 'thresholds_DOY_{:03d}_to_{:03d}_bin_{:02d}.h5'.format(DOY_start, DOY_end, DOY_bin)
+thresh_file = 'thresholds_DOY_{:03d}_to_{:03d}_bin_{:02d}_numSID_{:02d}.h5'.format(DOY_start, DOY_end, DOY_bin, 12)
 SID_file    = 'surfaceID_LosAngeles_{:03d}.nc'.format(DOY_end)
 print(test_scene, thresh_file, SID_file)
 
