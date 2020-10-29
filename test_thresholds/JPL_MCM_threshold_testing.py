5ad98510645fbcbd3daa58214d120f0a8106ff78
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-28 19:30:32 -0500

    d

--- test_thresholds/JPL_MCM_threshold_testing.py
+++ test_thresholds/JPL_MCM_threshold_testing.py
@@ -484,6 +484,7 @@ def get_test_determination(observable_level_parameter, observable_data,\
     #observable_level_parameter contains bins to query threshold database
 
     shape = observable_data.shape
+    print(observable_level_parameter.shape)
 
     #pick threshold for each pixel in x by y grid
     with h5py.File(threshold_path, 'r') as hf_thresholds:
