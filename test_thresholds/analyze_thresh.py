b670118a43dbefc65d0b31de68ee68f0a7a68115
vllgsbr2 <vllgsbr2@illinois.edu>
2020-10-30 14:48:38 -0500

    d

--- test_thresholds/analyze_thresh.py
+++ test_thresholds/analyze_thresh.py
@@ -171,33 +171,33 @@ def plot_thresh_hist_all_bins(num_land_SID):
     # thresholds        = []
     range_ = (0,1)
     num_bins  = 70
-    water =num_land_SID
-    glint=num_land_SID+1
-    snow=num_land_SID+2
+    water =num_land_SID+1
+    glint=num_land_SID+2
+    snow=num_land_SID+3
     for i, obs in enumerate(thresh_dict):
-        # if obs=='SVI' or obs=='Cirrus':
-        #     temp_thresh = np.copy(master_thresh[i])
-        #     if obs=='SVI':
-        #         range_ = (0,0.3)
-        #     else:
-        #         range_ = (0,0.15)
-        # elif obs == 'WI':
-        #     temp_thresh = np.copy(master_thresh[i,:,:,:,:,:glint])
-        # elif obs == 'NDVI':
-        #     temp_thresh = np.copy(master_thresh[i])
-        #     temp_thresh = np.concatenate((temp_thresh[:,:,:,:,:water].flatten(), temp_thresh[:,:,:,:,water:snow].flatten()))
-        #     range_ = (-0.5,0.5)
-        # elif obs == 'NDSI':
-        #     temp_thresh = np.copy(master_thresh[i,:,:,:,:,snow])
-        #     range_ = (0.3,1)
-        # elif obs == 'VIS_Ref':
-        #     temp_thresh = np.copy(master_thresh[i,:,:,:,:,:water])
-        #     range_ = (0,0.85)
-        # elif obs == 'NIR_Ref':
-        #     temp_thresh = np.copy(master_thresh[i,:,:,:,:,water])
-        #     range_ = (0,0.6)
-        # else:
-        #     pass
+        if obs=='SVI' or obs=='Cirrus':
+            temp_thresh = np.copy(master_thresh[i])
+            if obs=='SVI':
+                range_ = (0,0.3)
+            else:
+                range_ = (0,0.15)
+        elif obs == 'WI':
+            temp_thresh = np.copy(master_thresh[i,:,:,:,:,:glint])
+        elif obs == 'NDVI':
+            temp_thresh = np.copy(master_thresh[i])
+            temp_thresh = np.concatenate((temp_thresh[:,:,:,:,:water].flatten(), temp_thresh[:,:,:,:,water:snow].flatten()))
+            range_ = (-0.5,0.5)
+        elif obs == 'NDSI':
+            temp_thresh = np.copy(master_thresh[i,:,:,:,:,snow])
+            range_ = (0.3,1)
+        elif obs == 'VIS_Ref':
+            temp_thresh = np.copy(master_thresh[i,:,:,:,:,:water])
+            range_ = (0,0.85)
+        elif obs == 'NIR_Ref':
+            temp_thresh = np.copy(master_thresh[i,:,:,:,:,water])
+            range_ = (0,0.6)
+        else:
+            pass
 
         temp_thresh = master_thresh[i]
 
@@ -715,7 +715,7 @@ if __name__ == '__main__':
     # check_sunglint_flag_in_database()
     # check_sunglint_flag_in_grouped_cm_and_obs()
     # make_obs_hist_by_group('SVI')
-    num_land_SID=12
+    num_land_SID=11
     plot_thresh_hist_all_bins(num_land_SID)
 
 
