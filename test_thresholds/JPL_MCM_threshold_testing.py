6d1fd809ec1a8c0e82075b6f85cd2c91d0528f54
vllgsbr2 <vllgsbr2@illinois.edu>
2020-11-07 20:42:39 -0600

    d

--- test_thresholds/JPL_MCM_threshold_testing.py
+++ test_thresholds/JPL_MCM_threshold_testing.py
@@ -969,7 +969,7 @@ def MCM_wrapper(test_data_JPL_path, Target_Area_X, threshold_filepath,\
     DTT_NDVI_over_water = get_DTT_NDVI_Test_over_water(T[:,:,1] , observable_data[:,:,1], \
            Max_valid_DTT, Min_valid_DTT, fill_val_1, fill_val_2, fill_val_3)
     #where NDVI is over water use DTT_NDVI_over_water, leave the rest
-    water_SID = num_land_sfc_types_plus_coast+1
+    water_SID = num_land_sfc_types_plus_coast
     print('hello this is water sid: ', water_SID)
     water_idx = np.where(scene_type_identifier == water_SID)
     DTT_NDVI[water_idx] = DTT_NDVI_over_water[water_idx]
