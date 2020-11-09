087a2b3a65ae2985bee4b89e5052e6dc7087f72a
vllgsbr2 <vllgsbr2@illinois.edu>
2020-11-08 22:27:08 -0600

    d

--- test_thresholds/JPL_MCM_threshold_testing.py
+++ test_thresholds/JPL_MCM_threshold_testing.py
@@ -515,7 +515,7 @@ def get_test_determination(observable_level_parameter, observable_data,\
 
             thresholds[fillVal_idx[0]] = -999
             #mask over invalid SID with -999 in threshold array
-            thresholds[invalid_SID_idx[0],4] = -999
+            thresholds[invalid_SID_idx[0]] = -999
 
             #reshape to original dimensions
             thresholds = np.array(thresholds).reshape(shape)
