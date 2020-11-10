bfc0b0f4188f6021200c21a78f0a163902e435ec
vllgsbr2 <vllgsbr2@illinois.edu>
2020-11-10 14:22:35 -0600

    plot Kmeans SID # vs Composite Accuracy\nYears 2004/2010/2018

---
+++ test_thresholds/accur_by_numKmeansSID.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.animation as animation
+import configparser
+import h5py
+import sys
+# 'scene_Accuracy_DOY_bin_{:02d}'.format(DOY_bin)
+
+config_home_path = '/data/keeling/a/vllgsbr2/c/MAIA_thresh_dev/MAIA_CloudMask_Threshold_Development'
+config           = configparser.ConfigParser()
+config.read(config_home_path+'/test_config.txt')
+
+PTA           = config['current PTA']['PTA']
+PTA_path      = config['PTAs'][PTA]
+
+SID_accur = []
+
+for numKmeansSID in range(4,30):
+    scene_accur_home = '{}/{}/numKmeansSID_{:02d}'.format(PTA_path, config['supporting directories']['scene_accuracy'], numKmeansSID)
+    scene_accur_path = scene_accur_home + '/' + 'scene_ID_accuracy.h5'
+
+    scene_accurs = np.zeros((400,300,46))
+
+    plt.rcParams['font.size'] = 16
+
+    with h5py.File(scene_accur_path, 'r') as hf_scene_accur:
+        DOY_bins = list(hf_scene_accur.keys())
+        for i, DOY_bin in enumerate(DOY_bins):
+            scene_accurs[:,:,i] = hf_scene_accur[DOY_bin+'/MCM_accuracy'][()]*100
+
+    scene_accurs = np.mean(np.flatten(scene_accurs))
+    SID_accur.append(scene_accurs)
+
+plt.plot(np.arange(4,30), SID_accur)
+plt.title('Kmeans SID # vs Composite Accuracy\nYears 2004/2010/2018')
+
+plt.show()
