3110d98bbfed447a2463f67c4ecdee69b6c071ed
vllgsbr2 <vllgsbr2@illinois.edu>
2020-11-10 14:52:34 -0600

    d

--- test_thresholds/accur_by_numKmeansSID.py
+++ test_thresholds/accur_by_numKmeansSID.py
@@ -29,8 +29,9 @@ for numKmeansSID in range(4,30):
         DOY_bins = list(hf_scene_accur.keys())
         for i, DOY_bin in enumerate(DOY_bins):
             scene_accurs[:,:,i] = hf_scene_accur[DOY_bin+'/MCM_accuracy'][()]*100
+            
     scene_accurs[scene_accurs < 0] = np.nan
-    scene_accurs = np.nanmean(scene_accurs.flatten())
+    scene_accurs                   = np.nanmean(scene_accurs.flatten())
     SID_accur.append(scene_accurs)
     print('SID: ',numKmeansSID)
 
