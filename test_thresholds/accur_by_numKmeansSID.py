66cea0f86534c9a003d08694fedf97a3f1d3e10e
vllgsbr2 <vllgsbr2@illinois.edu>
2020-11-13 13:44:30 -0600

    plot line plot

--- test_thresholds/accur_by_numKmeansSID.py
+++ test_thresholds/accur_by_numKmeansSID.py
@@ -25,6 +25,36 @@ for i, a in enumerate(ax.flat):
         a.axis('off')
 
 for a, numKmeansSID in zip(ax.flat,range(4,30)):
+
+    # #where to find scene confusion matricies
+    # conf_matx_scene_dir = config['supporting directories']['conf_matx_scene']
+    # conf_matx_scene_dir = '{}/{}/numKmeansSID_{:02d}'.format(PTA_path, conf_matx_scene_dir, numKmeansSID)
+    # with h5py.File(conf_matx_scene_dir, 'r') as hf_confmatx:
+    #     confmatx_keys = np.array(list(hf_confmatx.keys()))
+    #     time_stamps   = [x[-12:] for x in confmatx_keys]
+    #     masks         = [x for x in confmatx_keys if x[:-13] == 'confusion_matrix_mask']
+    #     tables        = [x for x in confmatx_keys if x[:-13] == 'confusion_matrix_table']
+    #
+    #     #only take scenes with >=90% intersect with MAIA L2 grid
+    #     #then organize by cloud fraction
+    #     #record timestamps to use in accuracy graphs
+    #     scenes_gt_90_percent_intersect_L2_grid = []
+    #     scenes_by_CF = {'20':[], '40':[], '60':[], '80':[], '100':[]}
+    #     for time_stamp, table in zip(time_stamps, table):
+    #         table_scne_x = hf_confmatx[table][()]
+    #
+    #         L2_grid_size = 400*300
+    #         num_pixels_in_scene = table.sum()
+    #         if num_pixels_in_scene / L2_grid_size >= 0.9:
+    #             scenes_gt_90_percent_intersect_L2_grid.append(time_stamp)
+    #
+    #             #get cloud fraction CF
+    #             CF = table[:2].sum()/num_pixels_in_scene
+    #             for CF_key in scenes_by_CF:
+    #                 if CF <= int(CF_key):
+    #                     scenes_by_CF[CF_key].append(time_stamp)
+
+    #now get accuracy by DOY
     scene_accur_home = '{}/{}/numKmeansSID_{:02d}'.format(PTA_path, config['supporting directories']['scene_accuracy'], numKmeansSID)
     scene_accur_path = scene_accur_home + '/' + 'scene_ID_accuracy.h5'
 
@@ -45,7 +75,7 @@ for a, numKmeansSID in zip(ax.flat,range(4,30)):
     # # plt.hist(scene_accurs.flatten(), bins=20)
 
     scene_accurs = np.nanmean(scene_accurs.flatten())
-    a.set_title('SID {:02d};{:2.2f}'.format(numKmeansSID, scene_accurs))
+    a.set_title('SID {:02d};{:2.2f}%'.format(numKmeansSID, scene_accurs))
     print(scene_accurs)
     SID_accur.append(scene_accurs)
     print('SID: ',numKmeansSID)
@@ -53,13 +83,19 @@ for a, numKmeansSID in zip(ax.flat,range(4,30)):
 cb_ax = f.add_axes([0.93, 0.1, 0.02, 0.8])
 cbar = f.colorbar(im, cax=cb_ax)
 
+plt.figure(2)
+
+plt.plot(np.arange(4,30), SID_accur)
+plt.title('Kmeans SID # vs Composite Accuracy\nYears 2004/2010/2018')
+
 plt.show()
+#*******************************************************************************
 
-# plt.plot(np.arange(4,30), SID_accur)
-# plt.title('Kmeans SID # vs Composite Accuracy\nYears 2004/2010/2018')
-#
-# plt.show()
 
+
+
+
+#*******************************************************************************
 # scene_accur_home = '{}/{}/'.format(PTA_path, config['supporting directories']['scene_accuracy'])
 # scene_accur_path = scene_accur_home + 'scene_ID_accuracy.h5'
 #
