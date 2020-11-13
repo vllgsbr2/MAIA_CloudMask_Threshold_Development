0a744f5235e9979c1af3cb12144ca4f6fd1cad58
vllgsbr2 <vllgsbr2@illinois.edu>
2020-11-13 13:17:02 -0600

    take mean along axis 2

--- test_thresholds/accur_by_numKmeansSID.py
+++ test_thresholds/accur_by_numKmeansSID.py
@@ -40,7 +40,7 @@ for a, numKmeansSID in zip(ax.flat,range(4,30)):
 
     scene_accurs[scene_accurs < 0] = np.nan
     scene_accurs *= 100
-    im=a.imshow(scene_accurs[:,:,0], vmin=0, vmax=100)
+    im=a.imshow(np.nanmean(scene_accurs, axis=2), vmin=0, vmax=100)
     # # plt.hist(scene_accurs.flatten(), bins=20)
 
     scene_accurs = np.nanmean(scene_accurs.flatten())
