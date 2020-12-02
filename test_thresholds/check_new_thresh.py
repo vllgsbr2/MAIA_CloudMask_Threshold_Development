fe0128b9d9a9ee43a4d5f66443561692b3271a34
vllgsbr2 <vllgsbr2@illinois.edu>
2020-12-02 14:19:34 -0600

    d

--- test_thresholds/check_new_thresh.py
+++ test_thresholds/check_new_thresh.py
@@ -4,7 +4,7 @@ import h5py
 import os
 
 path = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/PTAs/LosAngeles/thresh_dev/thresholds/'
-thresh_files = [path+x for x in os.listdir(path) if x[0]=='t']
+thresh_files = [path+x for x in np.sort(os.listdir(path)) if x[0]=='t']
 
 for thresh in thresh_files:
     with h5py.File(thresh,'r')as hf_thresh:
@@ -14,4 +14,5 @@ for thresh in thresh_files:
         for ob in obs:
             obs_path = '{}/{}/{}'.format(TA[0],DOY[0],ob)
             thresh_temp = hf_thresh[obs_path][()]
-            print(obs_path, thresh_temp[thresh_temp != -999].size)
+            num_thresh = thresh_temp[thresh_temp != -999].shape
+            print(obs_path, num_thresh)
