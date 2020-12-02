25f1d4a6dc743ddfcb6a64ddd92e75714a45afbe
vllgsbr2 <vllgsbr2@illinois.edu>
2020-12-02 14:15:08 -0600

    check if threshold files are filled

---
+++ test_thresholds/check_new_thresh.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+import os
+
+path = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/PTAs/LosAngeles/thresh_dev/thresholds'
+thresh_files = [x for x in os.listdir(path) if x[0]=='t']
+
+for thresh in thresh_files:
+    with h5py.File(thresh,'r')as hf_thresh:
+        TA = list(hf_thresh.keys())
+        DOY = list(TA[0].keys())
+        obs = list(DOY[0].keys())
+        for ob in obs:
+            obs_path = '{}/{}/{}'.format(TA,DOY,ob)
+            thresh_temp = hf_thresh[obs_path][()]
+            print(thresh_temp[thresh_temp != -999].size)
