8e37ace220ed4a9d5dcc5aee4e3aa42de7bc083d
vllgsbr2 <vllgsbr2@illinois.edu>
2020-12-07 14:28:39 -0600

    d

--- test_thresholds/plt_MCM_output.py
+++ test_thresholds/plt_MCM_output.py
@@ -91,7 +91,7 @@ for time_stamp, test_data_JPL_path in zip(time_stamps, test_data_JPL_paths):
             divider = make_axes_locatable(a)
             cax = divider.append_axes('right', size='5%', pad=0.05)
             cbar = f.colorbar(im, cax=cax, orientation='vertical')
-            cbar.set_ticks(np.arange(15))
+            cbar.set_ticks(np.arange(0.5,14.5))
             SID_cbar_labels = ['0','1','2','3','4','5','6','7','8','9','10','coast', 'water', 'sunglint\nwater', 'snow\nice']
             cbar.set_ticklabels(SID_cbar_labels)
 
