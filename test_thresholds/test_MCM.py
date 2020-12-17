3fa05bded5080989f6ce0b3ad64119a3af2504f6
vllgsbr2 <vllgsbr2@illinois.edu>
2020-12-16 18:55:58 -0600

    d

--- test_thresholds/test_MCM.py
+++ test_thresholds/test_MCM.py
@@ -35,7 +35,7 @@ guangyu_home = '/data/gdi/c/gzhao1/MCM-thresholds/PTAs/LosAngeles/thresh_dev/thr
 # threshold_filepath = guangyu_home + 'OBthresholds_DOY_273_to_280_bin_34.h5'
 # test_scene = 'test_JPL_data_2019276.1805.h5'
 # test_scene = 'test_JPL_data_2006029.1910.h5'
-test_scene = 'test_JPL_data_2019287.1925.h5'
+# test_scene = 'test_JPL_data_2019287.1925.h5'
 # test_scene = 'test_JPL_data_2019089.1825.h5'
 DOY = int(test_scene[18:-8])
 DOY_bin = np.digitize(DOY, np.arange(8,376,8), right=True)
