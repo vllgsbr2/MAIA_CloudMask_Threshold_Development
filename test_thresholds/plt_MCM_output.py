9d2c81fb44aed27c215b8cfc4ffe87288bef6c7d
Javier <vllgsbr2@llinois.edu>
2020-12-29 15:39:21 -0600

    d

--- test_thresholds/plt_MCM_output.py
+++ test_thresholds/plt_MCM_output.py
@@ -161,7 +161,7 @@ for time_stamp, test_data_JPL_path in zip(time_stamps, test_data_JPL_paths):
             cax = f.add_axes([0.83, 0.11, 0.012, 0.24])
             cbar = f.colorbar(im_SID, cax=cax, orientation='vertical')
             cbar.set_ticks(np.arange(0.5,20.5))
-            SID_cbar_labels = ['0','1','2','3','4','5','6','7','8','9','10','C', 'W', 'SGW', 'SI']
+            SID_cbar_labels = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','C', 'W', 'SGW', 'SI']
             cbar.set_ticklabels(SID_cbar_labels)
             im_SID.cmap.set_under('r')
 
