cdea73caafb8bc5488a4b27dc605e46e625d7fa1
Javier <vllgsbr2@llinois.edu>
2020-12-29 15:43:56 -0600

    d

--- test_thresholds/plt_MCM_output.py
+++ test_thresholds/plt_MCM_output.py
@@ -156,11 +156,11 @@ for time_stamp, test_data_JPL_path in zip(time_stamps, test_data_JPL_paths):
 
         elif i==3:
             cmap = cm.get_cmap('ocean', 20)
-            im_SID = a.imshow(SID, vmin=0, vmax=21, cmap=cmap)
+            im_SID = a.imshow(SID, vmin=0, vmax=22, cmap=cmap)
             a.set_title('SID')
             cax = f.add_axes([0.83, 0.11, 0.012, 0.24])
             cbar = f.colorbar(im_SID, cax=cax, orientation='vertical')
-            cbar.set_ticks(np.arange(0.5,21.5))
+            cbar.set_ticks(np.arange(0.5,22.5))
             SID_cbar_labels = ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','C', 'W', 'SGW', 'SI']
             cbar.set_ticklabels(SID_cbar_labels)
             im_SID.cmap.set_under('r')
