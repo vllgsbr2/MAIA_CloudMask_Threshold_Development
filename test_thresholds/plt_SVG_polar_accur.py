e7a7037991add4a7a73fb925dd70799bb9f8969f
Javier <vllgsbr2@llinois.edu>
2020-12-30 16:29:43 -0600

    d

--- test_thresholds/plt_SVG_polar_accur.py
+++ test_thresholds/plt_SVG_polar_accur.py
@@ -79,11 +79,12 @@ for i, a in enumerate(ax.flat):
     SZA1 = np.rad2deg(np.arccos((i+4)/10))
     SZA2 = np.rad2deg(np.arccos(((i+4)+1)/10))
     a.set_title('SZA {:2.2f} - {:2.2f} [deg]'.format(SZA1, SZA2))
-    # a.set_rticks(np.arange(0,75,5))
+    a.set_rticks(np.arange(0,75,5))
+    a.grid()
     a.set_yticks(np.arange(0,80,10))
     a.set_yticklabels(np.arange(0,80,10))
     a.set_thetamax(180)
-    a.grid()
+
 
 cax = fig.add_axes([0.92, 0.23, 0.02, 0.5])#l,b,w,h
 cbar = fig.colorbar(im, cax=cax, ticks=np.arange(0,105,5))
