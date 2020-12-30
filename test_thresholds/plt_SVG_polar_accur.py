4c5a759b387f4ae8591311146cdbeccb0281046d
Javier <vllgsbr2@llinois.edu>
2020-12-30 16:02:52 -0600

    d

--- test_thresholds/plt_SVG_polar_accur.py
+++ test_thresholds/plt_SVG_polar_accur.py
@@ -71,20 +71,22 @@ fig, ax = plt.subplots(5,2, subplot_kw=dict(projection='polar'))
 
 
 for i, a in enumerate(ax.flat):
-    cmap = cm.get_cmap('plasma', 20)
-    a.set_thetagrids(np.arange(0,195,15))
-
-    im = a.pcolormesh(theta, r, weighted_accuracy_SVG[i,:,:],\
-                      cmap=cmap, vmin=0, vmax=100)
-    SZA1 = np.rad2deg(np.arccos(i/10))
-    SZA2 = np.rad2deg(np.arccos((i+1)/10))
-    a.set_title('SZA {:2.2f} - {:2.2f} [deg]'.format(SZA1, SZA2))
-    a.set_rticks(np.arange(0,75,5))
-    a.set_thetamax(180)
-    a.grid()
-
     if i<4:
         a.axis('off')
+    else:
+        cmap = cm.get_cmap('plasma', 20)
+        a.set_thetagrids(np.arange(0,195,15))
+
+        im = a.pcolormesh(theta, r, weighted_accuracy_SVG[i,:,:],\
+                          cmap=cmap, vmin=0, vmax=100)
+        SZA1 = np.rad2deg(np.arccos(i/10))
+        SZA2 = np.rad2deg(np.arccos((i+1)/10))
+        a.set_title('SZA {:2.2f} - {:2.2f} [deg]'.format(SZA1, SZA2))
+        a.set_rticks(np.arange(0,75,5))
+        a.set_thetamax(180)
+        a.grid()
+
+
 
 
 cax = fig.add_axes([0.95, 0.23, 0.02, 0.5])#l,b,w,h
