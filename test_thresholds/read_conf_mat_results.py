f8d54254b6261f3ff0ae009c354c89dc9e07ed26
Javier <vllgsbr2@llinois.edu>
2021-01-06 17:03:40 -0600

    d

--- test_thresholds/read_conf_mat_results.py
+++ test_thresholds/read_conf_mat_results.py
@@ -6,11 +6,11 @@ data = pd.read_csv('./conf_mat_results.csv')
 #now get group name
 #define paths for the three databases
 home = '/data/keeling/a/vllgsbr2/c/old_MAIA_Threshold_dev/PTAs/LosAngeles/results/conf_matx_group/numKmeansSID_16/'
-grouped_path = home + 'conf_matx_group_DOY_bin_{:02d}.h5'.format(DOY_bin)
 
 for DOY_bin in range(46):
+    grouped_path = home + 'conf_matx_group_DOY_bin_{:02d}.h5'.format(DOY_bin)
     with h5py.File(grouped_path, 'r') as hf_group:
         group_keys = list(hf_group.keys())
-        
+
     print(len(group_keys))
     print(data.shape)
