48aa692b12c46103447689dbf7657fa118864aa4
Javier <vllgsbr2@llinois.edu>
2021-01-06 19:29:49 -0600

    d

--- test_thresholds/analyze_thresh.py
+++ test_thresholds/analyze_thresh.py
@@ -555,8 +555,9 @@ def plot_thresh_vs_sfcID():
 
         a.boxplot(boxplot_thresh_obs_i, notch=False, sym='')
         a.set_xticks(np.arange(1,21))
-        a.set_xticklabels([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,'water','glint','snow'], rotation=45)
+        a.set_xticklabels([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,'water','glint','snow'], rotation=45, fontsize=16)
         a.set_title(obs)
+        a.set_yticklabels(fontsize=16)
 
         a_twin = a.twinx()
         a_twin = a_twin.plot(np.arange(1,21), sfcID_thresh_percent_change, color='r')#, vmax=100)
