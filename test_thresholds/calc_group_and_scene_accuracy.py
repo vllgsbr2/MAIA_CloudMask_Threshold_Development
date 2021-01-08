01c1bbdd62f33c5193b33f78fbfd183a4f0d23bc
Javier <vllgsbr2@llinois.edu>
2021-01-07 18:34:05 -0600

    d

--- test_thresholds/calc_group_and_scene_accuracy.py
+++ test_thresholds/calc_group_and_scene_accuracy.py
@@ -180,10 +180,11 @@ if __name__ == '__main__':
 
             for group, accur_num_samples in accuracy_of_groups.items():
                 group = '{}{:02d}'.format(group[:-2],i)
+                print(group)
                 hf_group_accur.create_group(group)
                 hf_group_accur[group].create_dataset('accuracy', data=accur_num_samples[0])
                 hf_group_accur[group].create_dataset('num_samples', data=accur_num_samples[1], dtype='int')
-                print(group)
+
             print('Group DOY: {} done'.format(i))
 
 
