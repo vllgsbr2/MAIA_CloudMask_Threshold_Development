d94cfccd323944aae83c3ec53957462ef7d45386
Javier <vllgsbr2@llinois.edu>
2021-01-10 22:23:20 -0600

    d

--- test_thresholds/plt_scene_accuracy_timeSeries.py
+++ test_thresholds/plt_scene_accuracy_timeSeries.py
@@ -41,7 +41,7 @@ with h5py.File(scene_accur_path, 'r') as hf_scene_accur:
     DOY_bins = list(hf_scene_accur.keys())
     for i, DOY_bin in enumerate(DOY_bins):
         scene_accurs[:,:,i] = hf_scene_accur[DOY_bin+'/MCM_accuracy'][()]
-        scene_num_samples[:,:,i] = hf_scene_accur[DOY_bin+'/num_samples'][()]
+        scene_num_samples[:,:,i] = np.nansum(hf_scene_accur[DOY_bin+'/num_samples'][()], axis=2)
 scene_accurs[scene_accurs == -999] = np.nan
 scene_num_samples[scene_num_samples == -999] = np.nan
 weighted_scene_accurs = np.nansum(scene_accurs*scene_num_samples, axis=2)/np.nansum(scene_num_samples, axis=2)
