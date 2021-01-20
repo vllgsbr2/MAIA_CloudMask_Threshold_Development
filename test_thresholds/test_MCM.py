6c24fb19ce048aa1e7b6472124cb15e3a3a693e8
Javier <vllgsbr2@llinois.edu>
2021-01-20 12:32:14 -0600

    d

--- test_thresholds/test_MCM.py
+++ test_thresholds/test_MCM.py
@@ -36,6 +36,7 @@ guangyu_home = '/data/gdi/c/gzhao1/MCM-thresholds/PTAs/LosAngeles/thresh_dev/thr
 # test_scene = 'test_JPL_data_2019276.1805.h5'
 # test_scene = 'test_JPL_data_2006029.1910.h5'
 test_scene = 'test_JPL_data_2019287.1925.h5'
+test_scene = 'test_JPL_data_2018188.1925.h5'
 # test_scene = 'test_JPL_data_2019089.1825.h5'
 DOY = int(test_scene[18:-8])
 DOY_bin = np.digitize(DOY, np.arange(8,376,8), right=True)
