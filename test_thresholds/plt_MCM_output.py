d850c839f3af5c5ffc2454880bc167ab0e2b3166
Javier <vllgsbr2@llinois.edu>
2021-01-21 15:41:24 -0600

    d

--- test_thresholds/plt_MCM_output.py
+++ test_thresholds/plt_MCM_output.py
@@ -83,7 +83,7 @@ f.subplots_adjust(bottom=bottom, right=right, top=top, wspace=wspace,\
 #                         '2011262.1855',\
 #                         '2011210.1920' ]
 
-thesisCaseTimestamps = ['2017.1920','2011210.1920']
+thesisCaseTimestamps = ['2017350.1850','2009244.1830']
 
 
 for time_stamp, test_data_JPL_path in zip(time_stamps, test_data_JPL_paths):
