03bcdd834f0e63e0f05d5dfb1d7637a58d605bbc
Javier <vllgsbr2@llinois.edu>
2021-02-03 16:29:11 -0600

    d

--- test_thresholds/plt_accur_by_OLP.py
+++ test_thresholds/plt_accur_by_OLP.py
@@ -146,9 +146,10 @@ def plot_accur_by_DOY():
     color = 'tab:cyan'
     ax1.set_ylabel('number of samples', color=color)  # we already handled the x-label with ax1
     ax1.semilogy(x, num_samples_list, color=color, linewidth=3)
-    # yticks = np.arange(1,11)*10**11
-    ax1.set_yticks(['10**11','','','','','','','','', '10**12'])
-    # ax1.set_yticklabels(yticks, style='sci')
+    yticks = np.arange(1,11)*10**11
+    y_ticklabels = ['10**11','','','','','','','','', '10**12']
+    ax1.set_yticks(yticks)
+    ax1.set_yticklabels(y_ticklabels)
 
     # ax1.set_ylim(10**11, 10**12)
     ax1.tick_params(axis='y', labelcolor=color)
