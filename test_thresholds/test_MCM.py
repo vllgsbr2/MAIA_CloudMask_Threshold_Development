83bedf465df5f21f15d7d148c7c940fe76d7b1e1
Javier Villegas <vllgsbr2@illinois.edu>
2021-02-24 17:13:01 -0600

    s

--- test_thresholds/test_MCM.py
+++ test_thresholds/test_MCM.py
@@ -115,83 +115,83 @@ make_output(Sun_glint_exclusion_angle,\
             scene_type_identifier)
 
 ################
-#plot obs without missing or unsued data from output file
-from JPL_MCM_threshold_testing import *
-with h5py.File('./MCM_output.HDF5', 'r') as hf_output:
-    R_band_04 = hf_output['Reflectance/band_04'][()]
-    R_band_05 = hf_output['Reflectance/band_05'][()]
-    R_band_06 = hf_output['Reflectance/band_06'][()]
-    R_band_09 = hf_output['Reflectance/band_09'][()]
-    R_band_12 = hf_output['Reflectance/band_12'][()]
-    R_band_13 = hf_output['Reflectance/band_13'][()]
-
-WI      = get_whiteness_index(R_band_06, R_band_05, R_band_04)
-NDVI    = get_NDVI(R_band_06, R_band_09)
-NDSI    = get_NDSI(R_band_05, R_band_12)
-VIS_Ref = get_visible_reflectance(R_band_06)
-NIR_Ref = get_NIR_reflectance(R_band_09)
-Cirrus  = get_cirrus_Ref(R_band_13)
-SVI     = get_spatial_variability_index(R_band_06, 400,300)
+# #plot obs without missing or unsued data from output file
+# from JPL_MCM_threshold_testing import *
+# with h5py.File('./MCM_output.HDF5', 'r') as hf_output:
+#     R_band_04 = hf_output['Reflectance/band_04'][()]
+#     R_band_05 = hf_output['Reflectance/band_05'][()]
+#     R_band_06 = hf_output['Reflectance/band_06'][()]
+#     R_band_09 = hf_output['Reflectance/band_09'][()]
+#     R_band_12 = hf_output['Reflectance/band_12'][()]
+#     R_band_13 = hf_output['Reflectance/band_13'][()]
+#
+# WI      = get_whiteness_index(R_band_06, R_band_05, R_band_04)
+# NDVI    = get_NDVI(R_band_06, R_band_09)
+# NDSI    = get_NDSI(R_band_05, R_band_12)
+# VIS_Ref = get_visible_reflectance(R_band_06)
+# NIR_Ref = get_NIR_reflectance(R_band_09)
+# Cirrus  = get_cirrus_Ref(R_band_13)
+# SVI     = get_spatial_variability_index(R_band_06, 400,300)
 
 from rgb_enhancement import *
 
 RGB = np.flip(BRFs[:,:,:3], 2)
 RGB[RGB==-999] = 0
 RGB = get_enhanced_RGB(RGB)
-plt.rcParams['font.size'] = 16
-f0, ax0 = plt.subplots(ncols=4, nrows=2,sharex=True, sharey=True)
-cmap='binary'
-Cirrus[Cirrus<0]=0
-im0 = ax0[0,0].imshow(WI     , cmap=cmap     , vmin=0, vmax = 0.6 )
-im1 = ax0[0,1].imshow(NDVI   , cmap='PRGn'   , vmin=-0.4, vmax=0.4)
-im2 = ax0[0,2].imshow(NDSI   , cmap='PRGn'   , vmin=-0.8 , vmax=0.8)
-im3 = ax0[0,3].imshow(VIS_Ref, cmap=cmap+'_r', vmin=0, vmax=0.8   )#, vmax=VIS_Ref.max())
-im4 = ax0[1,0].imshow(NIR_Ref, cmap=cmap+'_r', vmin=0, vmax=0.8  )#, vmax=NIR_Ref.max())
-im5 = ax0[1,1].imshow(SVI    , cmap=cmap+'_r', vmin=0, vmax=0.25)#, vmax=SVI.max())
-im6 = ax0[1,2].imshow(Cirrus , cmap=cmap+'_r', vmin=0, vmax=0.2   )#, vmax=1)
-
-ax0[1,3].imshow(RGB)
-
-im0.cmap.set_under('r')
-im1.cmap.set_under('r')
-im2.cmap.set_under('r')
-im3.cmap.set_under('r')
-im4.cmap.set_under('r')
-im5.cmap.set_under('r')
-# im6.cmap.set_under('r')
-
-cbar0 = f0.colorbar(im0, ax=ax0[0,0],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,WI.max()+0.2,0.2))
-cbar1 = f0.colorbar(im1, ax=ax0[0,1],fraction=0.046, pad=0.04)#)#, ticks = np.arange(-1,1.25,0.25))
-cbar2 = f0.colorbar(im2, ax=ax0[0,2],fraction=0.046, pad=0.04)#)#, ticks = np.arange(-1,1.1,0.1))
-cbar3 = f0.colorbar(im3, ax=ax0[0,3],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,VIS_Ref.max()+0.4,0.2))
-cbar4 = f0.colorbar(im4, ax=ax0[1,0],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,NIR_Ref.max()+0.1,0.1))
-cbar5 = f0.colorbar(im5, ax=ax0[1,1],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,SVI.max()+0.1,0.05))
-cbar6 = f0.colorbar(im6, ax=ax0[1,2],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,1.2,0.2))
-
-font_size = 16 # Adjust as appropriate.
-cbar0.ax.tick_params(labelsize=font_size)
-cbar1.ax.tick_params(labelsize=font_size)
-cbar2.ax.tick_params(labelsize=font_size)
-cbar3.ax.tick_params(labelsize=font_size)
-cbar4.ax.tick_params(labelsize=font_size)
-cbar5.ax.tick_params(labelsize=font_size)
-cbar6.ax.tick_params(labelsize=font_size)
-
-ax0[0,0].set_title('WI')
-ax0[0,1].set_title('NDVI')
-ax0[0,2].set_title('NDSI')
-ax0[0,3].set_title('0.65µm BRF')
-ax0[1,0].set_title('0.86µm BRF')
-ax0[1,1].set_title('SVI @ 0.65µm BRF')
-ax0[1,2].set_title('1.38µm BRF')
-ax0[1,3].set_title('BRF RGB')
-
-for a in ax0.flat:
-    a.set_xticks([])
-    a.set_yticks([])
-    a.axis('off')
-plt.show()
-sys.exit()
+# plt.rcParams['font.size'] = 16
+# f0, ax0 = plt.subplots(ncols=4, nrows=2,sharex=True, sharey=True)
+# cmap='binary'
+# Cirrus[Cirrus<0]=0
+# im0 = ax0[0,0].imshow(WI     , cmap='jet'     , vmin=0, vmax = 0.6 )
+# im1 = ax0[0,1].imshow(NDVI   , cmap='PRGn'   , vmin=-0.4, vmax=0.4)
+# im2 = ax0[0,2].imshow(NDSI   , cmap='PRGn'   , vmin=-0.8 , vmax=0.8)
+# im3 = ax0[0,3].imshow(VIS_Ref, cmap=cmap+'_r', vmin=0, vmax=0.8   )#, vmax=VIS_Ref.max())
+# im4 = ax0[1,0].imshow(NIR_Ref, cmap=cmap+'_r', vmin=0, vmax=0.8  )#, vmax=NIR_Ref.max())
+# im5 = ax0[1,1].imshow(SVI    , cmap='gist_earth', vmin=0, vmax=0.25)#, vmax=SVI.max())
+# im6 = ax0[1,2].imshow(Cirrus , cmap=cmap+'_r', vmin=0, vmax=0.2   )#, vmax=1)
+#
+# ax0[1,3].imshow(RGB)
+#
+# im0.cmap.set_under('r')
+# im1.cmap.set_under('r')
+# im2.cmap.set_under('r')
+# im3.cmap.set_under('r')
+# im4.cmap.set_under('r')
+# im5.cmap.set_under('r')
+# # im6.cmap.set_under('r')
+#
+# cbar0 = f0.colorbar(im0, ax=ax0[0,0],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,WI.max()+0.2,0.2))
+# cbar1 = f0.colorbar(im1, ax=ax0[0,1],fraction=0.046, pad=0.04)#)#, ticks = np.arange(-1,1.25,0.25))
+# cbar2 = f0.colorbar(im2, ax=ax0[0,2],fraction=0.046, pad=0.04)#)#, ticks = np.arange(-1,1.1,0.1))
+# cbar3 = f0.colorbar(im3, ax=ax0[0,3],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,VIS_Ref.max()+0.4,0.2))
+# cbar4 = f0.colorbar(im4, ax=ax0[1,0],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,NIR_Ref.max()+0.1,0.1))
+# cbar5 = f0.colorbar(im5, ax=ax0[1,1],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,SVI.max()+0.1,0.05))
+# cbar6 = f0.colorbar(im6, ax=ax0[1,2],fraction=0.046, pad=0.04)#)#, ticks = np.arange(0,1.2,0.2))
+#
+# font_size = 16 # Adjust as appropriate.
+# cbar0.ax.tick_params(labelsize=font_size)
+# cbar1.ax.tick_params(labelsize=font_size)
+# cbar2.ax.tick_params(labelsize=font_size)
+# cbar3.ax.tick_params(labelsize=font_size)
+# cbar4.ax.tick_params(labelsize=font_size)
+# cbar5.ax.tick_params(labelsize=font_size)
+# cbar6.ax.tick_params(labelsize=font_size)
+#
+# ax0[0,0].set_title('WI')
+# ax0[0,1].set_title('NDVI')
+# ax0[0,2].set_title('NDSI')
+# ax0[0,3].set_title('0.65µm BRF')
+# ax0[1,0].set_title('0.86µm BRF')
+# ax0[1,1].set_title('SVI @ 0.65µm BRF')
+# ax0[1,2].set_title('1.38µm BRF')
+# ax0[1,3].set_title('BRF RGB')
+#
+# for a in ax0.flat:
+#     a.set_xticks([])
+#     a.set_yticks([])
+#     a.axis('off')
+# plt.show()
+# sys.exit()
 #################
 
 #grab mod35 cm from input file
@@ -340,13 +340,13 @@ ax[1,2].imshow(DTT_Cirrus, cmap=cmap, vmin=vmin, vmax=vmax)
 
 ax[1,3].imshow(RGB)
 
-ax[0,0].set_title('DTT_WI')
-ax[0,1].set_title('DTT_NDVI')
-ax[0,2].set_title('DTT_NDSI')
-ax[0,3].set_title('DTT_VIS_Ref')
-ax[1,0].set_title('DTT_NIR_Ref')
-ax[1,1].set_title('DTT_SVI')
-ax[1,2].set_title('DTT_Cirrus')
+ax[0,0].set_title('DTT WI')
+ax[0,1].set_title('DTT NDVI')
+ax[0,2].set_title('DTT NDSI')
+ax[0,3].set_title('DTT 0.65µm BRF')
+ax[1,0].set_title('DTT 0.86µm BRF')
+ax[1,1].set_title('DTT SVI @ 0.65µm BRF')
+ax[1,2].set_title('DTT 1.38µm BRF')
 ax[1,3].set_title('BRF RGB')
 
 cb_ax = f.add_axes([0.93, 0.1, 0.02, 0.8])
