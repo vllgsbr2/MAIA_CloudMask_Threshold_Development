73c8ea31012569ee208ea254144b76ed07ea813d
Javier Villegas <vllgsbr2@illinois.edu>
2021-02-25 16:11:58 -0600

    d

--- test_thresholds/plt_SVG_polar_accur.py
+++ test_thresholds/plt_SVG_polar_accur.py
@@ -18,37 +18,37 @@ group_accur_path = group_accur_home + '/' + 'group_ID_accuracy.h5'
 filepath = group_accur_path
 
 
-s_list = np.zeros((10,12,15))
-num_samples_list=np.zeros((10,12,15))
-weighted_accuracy = np.zeros((10,12,15))
-with h5py.File(filepath, 'r') as hf:
-    bins = list(hf.keys())
-
-    for SZA in range(10):
-        for RAA in range(12):
-            for VZA in range(15):
-                #place to store for each bin
-                accuracy =[]
-                num_samples=[]
-                #find group names of bin combo in for loop step
-                bin_IDs = [x for x in bins if x[:40]=='confusion_matrix_cosSZA_{:02d}_VZA_{:02d}_RAZ_{:02d}'.format(SZA,VZA,RAA)]
-                #cycle through all the unque SVG bins (DOY and SID may change ofcourse)
-                for i, bin_ID in enumerate(bin_IDs):
-                    # print(SZA,VZA,RAA,bin_ID)
-                    accuracy.append(hf[bin_ID+'/accuracy'][()])
-                    # print(hf[bin_ID+'/accuracy'][()])
-                    num_samples.append(hf[bin_ID+'/num_samples'][()])
-                accuracy, num_samples = np.array(accuracy), np.array(num_samples)
-                sum_num_samples = np.sum(num_samples)
-                weighted_accuracy[SZA, RAA, VZA] = np.sum(accuracy*num_samples) / sum_num_samples
-                num_samples_list[SZA, RAA, VZA] = sum_num_samples
-        print('SZA bin: ', SZA)
-                # s_temp = np.array(accuracy)
-                # num_samples_temp = np.array(num_samples)
-                # # s_temp = s_temp[s_temp>=0]
-                # s_list[SZA,RAA,VZA] = np.nanmean(s_temp)*100
-                # num_samples_list[SZA,RAA,VZA] = np.nansum(np.array(num_samples))
-np.savez('./SVG_accur_data.npz', weighted_accuracy=weighted_accuracy, num_samples=num_samples_list)
+# s_list = np.zeros((10,12,15))
+# num_samples_list=np.zeros((10,12,15))
+# weighted_accuracy = np.zeros((10,12,15))
+# with h5py.File(filepath, 'r') as hf:
+#     bins = list(hf.keys())
+#
+#     for SZA in range(10):
+#         for RAA in range(12):
+#             for VZA in range(15):
+#                 #place to store for each bin
+#                 accuracy =[]
+#                 num_samples=[]
+#                 #find group names of bin combo in for loop step
+#                 bin_IDs = [x for x in bins if x[:40]=='confusion_matrix_cosSZA_{:02d}_VZA_{:02d}_RAZ_{:02d}'.format(SZA,VZA,RAA)]
+#                 #cycle through all the unque SVG bins (DOY and SID may change ofcourse)
+#                 for i, bin_ID in enumerate(bin_IDs):
+#                     # print(SZA,VZA,RAA,bin_ID)
+#                     accuracy.append(hf[bin_ID+'/accuracy'][()])
+#                     # print(hf[bin_ID+'/accuracy'][()])
+#                     num_samples.append(hf[bin_ID+'/num_samples'][()])
+#                 accuracy, num_samples = np.array(accuracy), np.array(num_samples)
+#                 sum_num_samples = np.sum(num_samples)
+#                 weighted_accuracy[SZA, RAA, VZA] = np.sum(accuracy*num_samples) / sum_num_samples
+#                 num_samples_list[SZA, RAA, VZA] = sum_num_samples
+#         print('SZA bin: ', SZA)
+#                 # s_temp = np.array(accuracy)
+#                 # num_samples_temp = np.array(num_samples)
+#                 # # s_temp = s_temp[s_temp>=0]
+#                 # s_list[SZA,RAA,VZA] = np.nanmean(s_temp)*100
+#                 # num_samples_list[SZA,RAA,VZA] = np.nansum(np.array(num_samples))
+# np.savez('./SVG_accur_data.npz', weighted_accuracy=weighted_accuracy, num_samples=num_samples_list)
 
 
 #-- Generate Data -----------------------------------------
@@ -64,6 +64,7 @@ dataset_names = data.files
 
 weighted_accuracy_SVG = data[dataset_names[0]]*100
 num_samples = data[dataset_names[1]]
+num_samples[num_samples<=0] = np.nan
 
 # print(num_samples)
 #plot
@@ -93,8 +94,8 @@ for i, a in enumerate(ax.flat):
     a.set_thetamax(180)
 
 
-# cax = fig.add_axes([0.92, 0.23, 0.02, 0.5])#l,b,w,h
-# cbar = fig.colorbar(im, cax=cax, ticks=np.arange(0,105,5))
+cax = fig.add_axes([0.92, 0.23, 0.02, 0.5])#l,b,w,h
+cbar = fig.colorbar(im, cax=cax, ticks=np.arange(0,105,5))
 
 plt.show()
 
