7591c21fffe63cbe7fa7d56e305bfea5898e8342
Javier Villegas <vllgsbr2@illinois.edu>
2021-02-27 17:04:07 -0600

    d

--- test_thresholds/plt__num_SID_cat_by_DOY.py
+++ test_thresholds/plt__num_SID_cat_by_DOY.py
@@ -33,35 +33,36 @@ import h5py
 import mpi4py.MPI as MPI
 import configparser
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+# comm = MPI.COMM_WORLD
+# rank = comm.Get_rank()
+# size = comm.Get_size()
+#
+# for r in range(size):
+#     if rank==r:
+#         DOY_bin = r
+DOY_bin = 0
+#count SGW and SI over all data from output file
+config_home_path = '/data/keeling/a/vllgsbr2/c/MAIA_thresh_dev/MAIA_CloudMask_Threshold_Development'
+config = configparser.ConfigParser()
+config.read(config_home_path+'/test_config.txt')
 
-for r in range(size):
-    if rank==r:
-        DOY_bin = r
-        #count SGW and SI over all data from output file
-        config_home_path = '/data/keeling/a/vllgsbr2/c/MAIA_thresh_dev/MAIA_CloudMask_Threshold_Development'
-        config = configparser.ConfigParser()
-        config.read(config_home_path+'/test_config.txt')
+PTA          = config['current PTA']['PTA']
+PTA_path     = config['PTAs'][PTA]
 
-        PTA          = config['current PTA']['PTA']
-        PTA_path     = config['PTAs'][PTA]
+#get output files
+filepath_output = PTA_path + '/' + config['supporting directories']['MCM_Output']+'/numKmeansSID_16'
+timestamps      = os.listdir(filepath_output)
+DOY_end         = DOY_bin*8
+DOY_start       = DOY_end - 7
+timestamps      = [x fox in timestamps if DOY_start<int(x[4:7])<=DOY_end]
+filepath_output = [filepath_output+'/'+x+'/MCM_Output.h5' for x in timestamps]
 
-        #get output files
-        filepath_output = PTA_path + '/' + config['supporting directories']['MCM_Output']+'/numKmeansSID_16'
-        timestamps      = os.listdir(filepath_output)
-        DOY_end         = DOY_bin*8
-        DOY_start       = DOY_end - 7
-        timestamps      = [x fox in timestamps if DOY_start<int(x[4:7])<=DOY_end]
-        filepath_output = [filepath_output+'/'+x+'/MCM_Output.h5' for x in timestamps]
+DOY_sfcID = np.zeros(20)
+for f in filepath_output:
+    with h5py.File(f, 'r') as hf_output:
+        SID = hf_output['Ancillary/scene_type_identifier'][()]
+        idx, x=np.unique(SID[SID>=0], return_counts=True)
+        idx=idx.astype(np.int)
+    DOY_sfcID[idx] += x
 
-        DOY_sfcID = np.zeros(20)
-        for f in filepath_output:
-            with h5py.File(f, 'r') as hf_output:
-                SID = hf_output['Ancillary/scene_type_identifier'][()]
-                idx, x=np.unique(SID[SID>=0], return_counts=True)
-                idx=idx.astype(np.int)
-            DOY_sfcID[idx] += x
-
-        np.savez('/data/keeling/a/vllgsbr2/c/DOY_sfcID_{:03d}.npz'.format(DOY_bin), DOY_sfcID=DOY_sfcID)
+np.savez('/data/keeling/a/vllgsbr2/c/DOY_sfcID_{:03d}.npz'.format(DOY_bin), DOY_sfcID=DOY_sfcID)
