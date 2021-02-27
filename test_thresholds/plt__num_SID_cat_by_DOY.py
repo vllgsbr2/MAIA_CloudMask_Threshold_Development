8d7c8d5a890e7f104c45ddb52b663d89c4d30f0e
Javier Villegas <vllgsbr2@illinois.edu>
2021-02-27 16:05:18 -0600

    d

---
+++ test_thresholds/plt__num_SID_cat_by_DOY.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot
+from netCDF4 import Dataset
+
+
+#get sfc IDs
+filepath_SID = PTA_path + '/' + config['supporting directories']['Surface_IDs']
+filepath_SID = [filepath_SID +'/'+ x for x in os.listdir(filepath_SID) if x[0]=='s']
+SID = np.zeros((400,300,46))
+DOY_sfcID = np.zeros((16,46))
+for i in range(46):
+    with Dataset(filepath_SID[i],'r') as nc_sfcID:
+        SID[:,:,i] = nc_sfcID.variables['surface_ID'][:,:]
+    DOY_sfcID[:,i] = np.unique(SID[:,:,i])
+    print('i')
+
+
+plt.imshow(DOY_sfcID, cmap='jet')
+plt.show()
