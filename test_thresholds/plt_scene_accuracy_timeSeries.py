485301fff07d8114aeda34ed2439a05b47299a9c
Javier Villegas <vllgsbr2@illinois.edu>
2021-04-04 22:37:39 -0500

    d

--- test_thresholds/plt_scene_accuracy_timeSeries.py
+++ test_thresholds/plt_scene_accuracy_timeSeries.py
@@ -77,11 +77,11 @@ weighted_scene_accurs = np.nansum(scene_accurs*scene_num_samples, axis=2)/np.nan
 # plt.ylim([85,100])
 # plt.xlabel('Julian Day of Year')
 # plt.ylabel('% Accuracy')
-print(scene_accurs.shape)
+
 f, ax = plt.subplots(nrows=6, ncols=8)
 for i, a in enumerate(ax.flat):
-    print(i)
-    a.imshow(scene_accurs[:,:,i], vmin=0,vmax=100,cmap=cm.get_cmap('plasma', 20))
+    if i<=45:
+        a.imshow(scene_accurs[:,:,i], vmin=0,vmax=100,cmap=cm.get_cmap('plasma', 20))
     a.set_xticks([])
     a.set_yticks([])
 
