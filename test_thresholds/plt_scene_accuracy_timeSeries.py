61bd351c9232cd64174101c2841459787024eb68
Javier Villegas <vllgsbr2@illinois.edu>
2021-04-04 23:06:58 -0500

    d

--- test_thresholds/plt_scene_accuracy_timeSeries.py
+++ test_thresholds/plt_scene_accuracy_timeSeries.py
@@ -38,7 +38,7 @@ scene_num_samples[scene_num_samples == -999] = np.nan
 # {'DJF': [42.0, 6.0], 'MAM': [7.0, 18.0], 'JJA': [19.0, 29.0], 'SON': [30.0, 41.0]}
 
 DOY_to_season_dict = {'DJF': [42, 6], 'MAM': [7, 18], 'JJA': [19, 29], 'SON': [30, 41]}
-for key in DOY_to_season_dict:
+for i, key in enumerate(DOY_to_season_dict):
     start, end = DOY_to_season_dict[key]
     weighted_scene_accurs_by_season[:,:,i] = np.nansum(scene_accurs[:,:,start:end]*scene_num_samples[:,:,start:end], axis=2)/\
                                              np.nansum(scene_num_samples[:,:,start:end], axis=2)*100
